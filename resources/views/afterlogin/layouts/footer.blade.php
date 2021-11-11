9da28087ec8918825566e63c6c69ae5c31b14534
sakshisharma <sakshi@ahom.tech>
2021-11-11 19:51:06 +0530

    dashboard issue resolved

--- resources/views/afterlogin/layouts/footer.blade.php
+++ resources/views/afterlogin/layouts/footer.blade.php
@@ -900,20 +900,19 @@
             }
             var chapters = [];
             var chapters = $('input[name="chapters[]"]').length;
-            /*alert(chapters);
-            alert(limit);*/
+            
             if (chapters < limit) {
                 $('#limit_error').html('Select minimum ' + limit + ' chapter for planner.');
-                setTimeout(function() {
-                    $('#limit_error').fadeOut('slow');
-                }, 5000);
+                /*setTimeout(function() {
+                    $('#limit_error').fadeOut('fast');
+                }, 5000);*/
                 return false;
             }
             if (chapters > limit) {
                 $('#limit_error').html('Select minimum ' + limit + ' chapter for planner.');
-                setTimeout(function() {
-                    $('#limit_error').fadeOut('slow');
-                }, 5000);
+                /*setTimeout(function() {
+                    $('#limit_error').fadeOut('fast');
+                }, 5000);*/
                 return false;
             }
 
