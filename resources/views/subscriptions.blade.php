bb7c983c0db57180d2eafc9bffc875b643fb27d3
dineshrawat9568 <dinesh@ahom.tech>
2022-02-01 11:10:03 +0530

    subscription end foreach issue

--- resources/views/subscriptions.blade.php
+++ resources/views/subscriptions.blade.php
@@ -261,26 +261,28 @@ $user_exam_id = $userData->grade_id;
 
                         </div>
                     </div>
-                    @endif
-
-                    @endforeach
-                    @endif
-                    @endif
-
                 </div>
-                <!-- </div> -->
+                @endif
+                @endif
+
+                @endforeach
+                @endif
+                @endif
+
             </div>
+            <!-- </div> -->
         </div>
     </div>
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js" integrity="sha512-UdIMMlVx0HEynClOIFSyOrPggomfhBKJE28LKl8yR3ghkgugPnG6iLfRfHwushZl1MOPSY6TsuBDGPK2X4zYKg==" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js" integrity="sha512-6Uv+497AWTmj/6V14BsQioPrm3kgwmK9HYIyWP+vClykX52b0zrDGP7lajZoIY1nNlX4oQuh7zsGjmF7D0VZYA==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js" integrity="sha512-UdIMMlVx0HEynClOIFSyOrPggomfhBKJE28LKl8yR3ghkgugPnG6iLfRfHwushZl1MOPSY6TsuBDGPK2X4zYKg==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js" integrity="sha512-6Uv+497AWTmj/6V14BsQioPrm3kgwmK9HYIyWP+vClykX52b0zrDGP7lajZoIY1nNlX4oQuh7zsGjmF7D0VZYA==" crossorigin="anonymous"></script>
 
-    <script type="text/javascript" src="{{URL::asset('public/js/jquery-3.2.1.slim.min.js')}}"></script>
-    <script type="text/javascript" src="{{URL::asset('public/js/popper.min.js')}}"></script>
-    <script type="text/javascript" src="{{URL::asset('public/js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('public/js/jquery-3.2.1.slim.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('public/js/popper.min.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('public/js/bootstrap.min.js')}}"></script>
 
 
-    @endsection
+@endsection
