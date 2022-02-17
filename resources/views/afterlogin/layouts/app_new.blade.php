c09e6b02e6e6a13fd5f1ca27cad5c35a6b0a6edd
dineshrawat9568 <dinesh@ahom.tech>
2022-02-17 17:13:18 +0530

    subscription logout issue

--- resources/views/afterlogin/layouts/app_new.blade.php
+++ resources/views/afterlogin/layouts/app_new.blade.php
@@ -19,10 +19,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <!--  <link rel="stylesheet" href="css/style.css"> -->
+
+    <link rel="stylesheet" href="{{URL::asset('public/css/style.css')}}">
+
     <link rel="stylesheet" href="{{URL::asset('public/after_login/new_ui/css/style.css')}}">
     <link rel="stylesheet" href="{{URL::asset('public/after_login/new_ui/css/exam-analytics.css')}}">
     <link rel="stylesheet" href="{{URL::asset('public/after_login/new_ui/css/exampage.css')}}">
     <link rel="stylesheet" href="{{URL::asset('public/after_login/new_ui/css/responsive.css')}}">
+
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
 </head>
 
