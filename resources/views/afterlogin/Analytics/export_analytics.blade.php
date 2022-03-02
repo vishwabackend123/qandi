f39a3e3fe1d0bbe289c5fda1ee291f85f4c9e51e
Amit <sakshi707>
2022-03-02 17:28:36 -0500

    tooltip on export_analytics

--- resources/views/afterlogin/Analytics/export_analytics.blade.php
+++ resources/views/afterlogin/Analytics/export_analytics.blade.php
@@ -14,7 +14,7 @@ $userData = Session::get('user_data');
             <div class="row">
                 <div class="col-lg-9 mx-auto">
 
-                    <div class="bg-white">
+                    <div class="bg-white dashboard-cards-block">
                         <div class="report-block1 p-4">
                             <div class="
                       d-flex
@@ -36,6 +36,14 @@ $userData = Session::get('user_data');
                                 <div class="row">
                                     <div class="mx-auto col-md-10">
                                         <div class="bg-white shadow-lg p-5 report-analysis-block">
+                                            <small>
+                                                <i class="fa  fa-info"></i>
+                                                <p>
+                                                    <span><img style="width:34px;" src="{{URL::asset('public/after_login/new_ui/images/cross.png')}}"></span>
+                                                    <!-- <label>About MyQ Today</label> -->
+                                                    A score derived from the detailed analysis of your test patterns that gives a clear understanding of your current level of preparation in comparison to an ideal one. Measure your real-time probability of reaching the goal with your current pattern of preparation. Set your goal!
+                                                </p>
+                                            </small>
                                             <div class="d-flex align-items-center border-bottom pb-4">
                                                 <div>
                                                     <h1 class="reportHeading">
@@ -93,7 +101,13 @@ $userData = Session::get('user_data');
                                 </span>
                             </div>
                             <div id="myTabContent" class="bg-white shadow-lg p-3 mt-5">
-                                
+                                <small>
+                                    <i class="fa  fa-info"></i>
+                                    <p>
+                                        <span><img style="width:34px;" src="{{URL::asset('public/after_login/new_ui/images/cross.png')}}"></span>
+                                        This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
+                                    </p>
+                                </small>
                                 <h5 class="dashboard-title mb-3">Subject Performance </h5>
                                 @if(!empty($subProf))
                                 @foreach($subProf as $key=>$sub)
@@ -184,6 +198,13 @@ $userData = Session::get('user_data');
                             </div>
 
                             <div class="bg-white shadow-lg p-3 h-100 px-5 mt-3 text-center">
+                                <small>
+                                    <i class="fa  fa-info"></i>
+                                    <p>
+                                        <span><img style="width:34px;" src="{{URL::asset('public/after_login/new_ui/images/cross.png')}}"></span>
+                                        In a limited duration test, it is absolutely essential to manage your time and use it wisely to smartly choose the right questions to attempt. This will greatly increase your chances of achieving the magic score. Invest your time wisely!
+                                    </p>
+                                </small>
                                 <p class="text-uppercase fw-bold text-start">
                                     Time Management
                                 </p>
@@ -191,6 +212,13 @@ $userData = Session::get('user_data');
                                 <!--p class="text-center text-danger mt-3"><small>Investing your time in correctly answering questions is the key to success.</small></p-->
                             </div>
                             <div class="bg-white shadow-lg p-3 h-100 mt-3 px-5">
+                                <small>
+                                    <i class="fa  fa-info"></i>
+                                    <p>
+                                        <span><img style="width:34px;" src="{{URL::asset('public/after_login/new_ui/images/cross.png')}}"></span>
+                                        Keep your average time spent on each question low by allocating appropriate time to questions based on their difficulty. Lowering this average and add miles to your success!
+                                    </p>
+                                </small>
                                 <p class="text-uppercase fw-bold text-start">
                                     Average Time Spent on each Question (Last Week)
                                 </p>
@@ -198,6 +226,13 @@ $userData = Session::get('user_data');
                                 <!--p class="text-center text-danger mt-3 px-5"><small>Lowering this average will add miles to your success journey</small></p-->
                             </div>
                             <div class="bg-white shadow-lg p-3 px-5 mt-3">
+                                <small>
+                                    <i class="fa  fa-info"></i>
+                                    <p>
+                                        <span><img style="width:34px;" src="{{URL::asset('public/after_login/new_ui/images/cross.png')}}"></span>
+                                        It is not always about how many and how fast but how accurate you are in answering within the limited time. Be informed about how you are making efficient use of your time on the right questions. Strategize better for your next test!
+                                    </p>
+                                </small>
                                 <p class="text-uppercase fw-bold text-start">
                                     Accuracy Percentage (Last Week)
                                 </p>
@@ -470,5 +505,15 @@ $userData = Session::get('user_data');
             color: '#6ec986',
         }]
     });
+
+    $(document).ready(function() {
+        $(".dashboard-cards-block .bg-white>small i").click(function() {
+            $(this).siblings("p").show();
+        });
+        $(".dashboard-cards-block .bg-white>small p>span").click(function() {
+            $(this).parent("p").hide();
+        });
+    });
 </script>
+
 @endsection
