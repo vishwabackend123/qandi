455a1f94f37dd0b0f81f909a5fda4c21d039869b
vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-03-14 11:32:33 +0530

    review screen containt change

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redirect;
 
 use App\Http\Traits\CommonTrait;
-
+use Illuminate\Support\Facades\Log;
 class ReferralController extends Controller
 {
     //
@@ -21,76 +21,88 @@ class ReferralController extends Controller
     public function store_referral_friend(Request $request)
     {
 
-        $userData = Session::get('user_data');
-        $user_id = $userData->id;
-        $exam_id = $userData->grade_id;
-        $referrals = (isset($request->refer_emails) && !empty($request->refer_emails)) ? $request->refer_emails : '';
-        $referrals_code = (isset($request->refer_code) && !empty($request->refer_code)) ? $request->refer_code : '';
-        $inputjson = [
-            "student_id" => $user_id,
-            "exam_id" => $exam_id,
-            "email" => $referrals,
-            "student_refer_by" => $referrals_code,
-        ];
-        $request = json_encode($inputjson);
+        try {
+            $userData = Session::get('user_data');
+            $user_id = $userData->id;
+            $exam_id = $userData->grade_id;
+            $referrals = (isset($request->refer_emails) && !empty($request->refer_emails)) ? $request->refer_emails : '';
+            $referrals_code = (isset($request->refer_code) && !empty($request->refer_code)) ? $request->refer_code : '';
+            $inputjson = [
+                "student_id" => $user_id,
+                "exam_id" => $exam_id,
+                "email" => $referrals,
+                "student_refer_by" => $referrals_code,
+            ];
+            $request = json_encode($inputjson);
 
-        $api_URL = env('API_URL');
-        $curl_url = $api_URL . 'api/insert-referr-student';
+            $api_URL = env('API_URL');
+            $curl_url = $api_URL . 'api/insert-referr-student';
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $curl_url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FAILONERROR => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $request,
-            CURLOPT_HTTPHEADER => array(
-                "cache-control: no-cache",
-                "content-type: application/json",
-            ),
-        ));
-        $response_json = curl_exec($curl);
-        $err = curl_error($curl);
-        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $curl_url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FAILONERROR => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => $request,
+                CURLOPT_HTTPHEADER => array(
+                    "cache-control: no-cache",
+                    "content-type: application/json",
+                ),
+            ));
+            $response_json = curl_exec($curl);
+            $err = curl_error($curl);
+            $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            curl_close($curl);
 
-        if ($httpcode == 200 || $httpcode == 201) {
-            return $response_json;
-        } else {
-            return json_encode(array('success' => false, 'message' => 'Email already referred'));
+            if ($httpcode == 200 || $httpcode == 201) {
+                return $response_json;
+            } else {
+                return json_encode(array('success' => false, 'message' => 'Email already referred'));
+            }
+        }
+        catch(\Exception $e)
+        {
+            Log::info($e->getMessage());
         }
     }
 
     public function referral_signup($referral_code)
     {
-        $api_URL = env('API_URL');
-        $curl_url = $api_URL . 'api/get-referr-student/' . $referral_code;
-        $curl = curl_init();
+        try {
+            $api_URL = env('API_URL');
+            $curl_url = $api_URL . 'api/get-referr-student/' . $referral_code;
+            $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $curl_url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $curl_url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
 
-        $response = curl_exec($curl);
+            $response = curl_exec($curl);
 
-        curl_close($curl);
-        $refDecode =  json_decode($response);
-        $referral_email = isset($refDecode[0]->email) ? $refDecode[0]->email : '';
-        if (isset($referral_email) && !empty($referral_email)) {
-            return view('auth.register', compact('referral_email','referral_code'));
-        } else {
-            return abort(404);
+            curl_close($curl);
+            $refDecode =  json_decode($response);
+            $referral_email = isset($refDecode[0]->email) ? $refDecode[0]->email : '';
+            if (isset($referral_email) && !empty($referral_email)) {
+                return view('auth.register', compact('referral_email','referral_code'));
+            } else {
+                return abort(404);
+            }
+         }
+        catch(\Exception $e)
+        {
+            Log::info($e->getMessage());
         }
     }
 }
