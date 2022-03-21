82ebf7135eaed859cc9d1af90819a15125c6b7a8
dineshrawat9568 <dinesh@ahom.tech>
2022-03-21 17:37:40 +0530

    custom exam test issue

--- resources/views/afterlogin/ExamCustom/exam_custom.blade.php
+++ resources/views/afterlogin/ExamCustom/exam_custom.blade.php
@@ -134,7 +134,7 @@ $userData = Session::get('user_data');
                       </div>
 
                       <span class="slbs-link mx-3">
-                        <a class="expand-custom expandTopicCollapse" aria-controls="chapter_{{$chapters->chapter_id}}" data-bs-toggle="collapse" href="#chapter_{{$chapters->chapter_id}}" role="button" aria-expanded="false" value="Expand to topics" onclick="show_topic('{{$chapters->chapter_id}}')"><span id="expand_topic_{{$chapters->chapter_id}}">Expand to Topics</span></a></span>
+                        <a class="expand-custom expandTopicCollapse" aria-controls="chapter_{{$chapters->chapter_id}}" data-bs-toggle="collapse" href="#chapter_{{$chapters->chapter_id}}" role="button" aria-expanded="false" value="Expand to topics" onclick="show_topic('{{$chapters->chapter_id}}')"><span id="expand_topic_{{$chapters->chapter_id}}">Expand to topics</span></a></span>
 
                       <div class="d-flex px-4">
                         <button class="btn btn-light ms-auto text-danger rounded-0" id="dropdownMenuLink-topic" data-bs-toggle="dropdown" aria-expanded="false" title="Topics Filter">
@@ -564,18 +564,6 @@ $userData = Session::get('user_data');
 <script>
   $('.expandTopicCollapse').click(function() {
     $(this).parents('.ClickBack').toggleClass('newelement');
-    var scrollpas=$('.scroll-div').scrollTop();
-      var blockpos=$(this).offset().top;
-      var scrollblock=$( $(this).attr('href') ).offset().top;
-      if( scrollpas>0){
-        $('.scroll-div').animate({
-          scrollTop:scrollblock+scrollpas-blockpos 
-        }, 500);
-      }else{
-        $('.scroll-div').animate({
-          scrollTop:scrollblock-blockpos
-        }, 500);
-      }
 
   });
 </script>
