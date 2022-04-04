d121872b6c6a0b0f6c649d941851a622df8fd72e
My Name <sakshi707>
2022-04-04 14:14:14 +0530

    404 page html

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,23 +6,15 @@
     <title>404 Custom Error Page Example</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <style>
-        .btn.btn-backtopage {
-        background-color: #e9e9e9;
-        min-width: 180px;
-        border: 0;
-        font-size: 14px;
-        padding: 10px;
-        font-weight: 600;
-        box-shadow: none!important;
-    }
+        .btn.btn-backtopage {font-weight: 500;background-color: #00bdff;min-width: 190px;border-radius: 30px;font-size: 14px;padding: 12px;box-shadow: none!important;color:#fff;}
     </style>
 </head>
 <body>
-    <div style="padding:20px 20px 10px;"> <img  src="{{URL::asset('public/images_new/QI_Logo.gif')}}" style="width: 50px;"> </div>
-    <div class="error-page text-center">
-        <img  src="{{URL::asset('public/after_login/new_ui/images/404-image.jpg')}}" style="max-width:400px;width:100%;">
-        <div class="text-center" style="margin-top: -40px;">
-            <p style="color: rgba(35,31,32,0.5);font-size: 18px;font-weight: 500;margin-bottom: 25px;">PAGE NOT FOUND</p>
+    <div style="padding:20px;"> <img  src="{{URL::asset('public/images_new/QI_Logo.gif')}}" style="width: 50px;"> </div>
+    <div class="error-page text-center" style="padding: 0 15px;">
+        <img  src="{{URL::asset('public/after_login/new_ui/images/404-image.png')}}" style="max-width:500px;width:100%;margin-bottom:25px;">
+        <div class="text-center">
+            <p style="margin-bottom: 30px;font-size: 30px;color: #231f20;font-weight:bold;">SORRY! THIS PAGE WAS LOST</p>
             <a href="{{url('/dashboard')}}" class="btn btn-backtopage">Back To Dashboard</a>
         </div>
     </div>
