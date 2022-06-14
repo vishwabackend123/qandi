b5fbdcf7cb00a361870836e65b337aeb768c7c9b
My Name <sakshi707>
2022-06-14 14:33:10 +0530

    previous year exam html

--- resources/views/afterlogin/PreviousYearExam/index.blade.php
+++ resources/views/afterlogin/PreviousYearExam/index.blade.php
@@ -38,13 +38,16 @@ $userData = Session::get('user_data');
                                 <li class="nav-item" role="presentation">
                                     <a class="nav-link all_div active" id="Mathematics-tab" data-bs-toggle="tab" href="#attempted" role="tab" aria-controls="attempted" aria-selected="true">Previous year exam</a>
                                 </li>
+                                 <li class="nav-item" role="presentation">
+                                    <a class="nav-link all_div" id="attempted-tab" data-bs-toggle="tab" href="#attempted" role="tab" aria-controls="attempted" aria-selected="true">Attempted</a>
+                                </li>
                             </ul>
                         </div>
                         <!--scroll-mobile-->
                         <div class="tab-content cust-tab-content" id="myTabContent">
                             <div class="tab-pane fade show active" id="open" role="tabpanel" aria-labelledby="open-tab">
                                 <div class="year_filter  p-3 mt-1">
-                                    <label style="font-weight: 500;color: #6b707f;">Year Filter:</label>
+                                    <label style="font-weight:600;">Test your preparedness with Past year exam papers </label>
                                     @php
                                     $latest_year = date('Y');
                                     @endphp
@@ -58,7 +61,57 @@ $userData = Session::get('user_data');
                                         @endif
                                     </select>
                                 </div>
-                                <div class="">
+                                <div class="compLeteS all-rlt {{$sche->subject_name}}-rlt exam_mode_{{$sche->exam_mode}}" id="active_click_{{$sche->id}}">
+                                    <div class="ClickBack d-md-flex align-items-center justify-content-between bg-white   listing-details w-100 flex-wrap result-list-table">
+                                        <div class="d-flex align-items-start justify-content-between result-list-head">
+                                            <h4 class="m-lg-0 p-0"> Previous year Paper NEET 13 June 2022
+                                            </h4>
+                                            <p class="m-0 p-0">13 June 2022</p>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-center morning-slot">
+                                            <span class="slbs-link me-lg-5 me-2">
+                                                <a class="expand-custom expandTopicCollapseAttempt" aria-controls="chapter_{{$sche->id}}" data-bs-toggle="collapse" href="#chapter_{{$sche->id}}" role="button" aria-expanded="true" value="Expand to Topics" id="clicktopic_{{$sche->id}}">
+                                                    <span class="hideallexpend" id="expand_topic_{{$sche->id}}" data-id="chapter_{{$sche->id}}">
+                                                        <i class="fa fa-arrow-down"></i>
+                                                        Show Details
+                                                    </span>
+                                                </a>
+                                            </span>
+                                        </div>
+                                        <div class="result-list-btns">
+                                            <button class="custom-btn-gray"><i class="fa fa-pencil-square-o"></i> TAKE TEST</button>
+                                        </div>
+                                    </div>
+                                    <div class="collapse" id="chapter_{{$sche->id}}">
+                                        <div class="p-4 pb-4 d-md-flex justify-content-between full-syllabus align-items-center">
+                                            <div class="d-flex justify-content-between align-items-center paper-summery pe-5">
+                                                <div class="paper-sub">
+                                                    <small>No. Of Questions</small>
+                                                    <span>5 MCQ <b style="font-weight:normal;">Questions</b></span>
+                                                </div>
+                                                <div class="paper-sub">
+                                                    <small>Duration</small>
+                                                    <span>30 <b style="font-weight:normal;">Mins</b></span>
+                                                </div>
+                                                <div class="paper-sub">
+                                                    <small>Marks</small>
+                                                    <span>200</span>
+                                                </div>
+                                                <div class="paper-sub">
+                                                    <small>Subjects</small>
+                                                    <span style="word-break: keep-all;">Physics, Chemistry & Maths</span>
+                                                </div>
+                                            </div>
+                                            <div class="score-show text-md-center">
+                                                <div class="paper-sub">
+                                                    <small>Score</small>
+                                                    <span></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- <div class="">
                                     @if(!empty($upcomming_live_exam))
 
                                     <div class="scroll-div-live-exm p-4 listing-details pb-0 mb-3 pt-0">
@@ -85,7 +138,7 @@ $userData = Session::get('user_data');
                                         <h5>No series available.</h5>
                                     </div>
                                     @endif
-                                </div>
+                                </div> -->
 
                             </div>
 
