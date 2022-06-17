5d2d0eb5baeadb56ea96924167621b6c2f4803cc
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-06-17 15:13:31 +0530

    hover issue

--- resources/views/afterlogin/dashboard.blade.php
+++ resources/views/afterlogin/dashboard.blade.php
@@ -643,6 +643,7 @@ $userData = Session::get('user_data');
     <div class="loader-block" style="display:none;">
         <img src="{{URL::asset('public/after_login/new_ui/images/loader.gif')}}">
     </div>
+    <input type="hidden" value="{{$mouseEvent}}" id="mouseEvent">
     <!-- Footer Section -->
     @include('afterlogin.layouts.footer_new')
     <!-- footer Section end  -->
@@ -885,6 +886,14 @@ $userData = Session::get('user_data');
     </script>
     <script>
         /* Score Pie Chart */
+        var mouseval = $('#mouseEvent').val();
+        var boolval = '';
+        if(mouseval == '1')
+        {
+            boolval = false;
+        }else if (mouseval == '2') {
+            boolval = true;
+        }
         Highcharts.chart('scorecontainer', {
             chart: {
                 height: 160,
@@ -937,6 +946,9 @@ $userData = Session::get('user_data');
                     endAngle: 140,
                     center: ['50%', '50%'],
                     size: '100%'
+                },
+                series: {
+                    enableMouseTracking: boolval
                 }
             },
             series: [{
