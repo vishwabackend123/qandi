136da2f0d1cc1ea88bc1f7bc163fdd583eeaa658
dinesh@ahom.tech-at-225208115870 <dinesh@ahom.tech-at-225208115870>
2022-06-28 18:55:33 +0530

    on exam inspect disable

--- resources/views/afterlogin/PreviousYearExam/previousYearExam.blade.php
+++ resources/views/afterlogin/PreviousYearExam/previousYearExam.blade.php
@@ -548,17 +548,17 @@ $questtype='radio';
     $(document).ready(function() {
 
         /* mouse rightclick */
-        /*  document.oncontextmenu = function() {
-             return false;
-         };
+        document.oncontextmenu = function() {
+            return false;
+        };
 
-         $(document).mousedown(function(e) {
-             if (e.button == 2) {
+        $(document).mousedown(function(e) {
+            if (e.button == 2) {
 
-                 return false;
-             }
-             return true;
-         }); */
+                return false;
+            }
+            return true;
+        });
         /* mouse rightclick */
 
         document.onkeydown = function(e) {
--- resources/views/afterlogin/layouts/app_new.blade.php
+++ resources/views/afterlogin/layouts/app_new.blade.php
@@ -44,7 +44,8 @@
     <script type="text/javascript" src="{{URL::asset('public/js/bootstrap.min.js')}}"></script>
 
     <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML-full"></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script>
         $(window).on('load', function() {
             if ($("#welcomeModal").length > 0) {
@@ -72,48 +73,12 @@
 
 
         /* no back prevention code */
-        (function(global) {
-
-            if (typeof(global) === "undefined") {
-                throw new Error("window is undefined");
-            }
-
-            var _hash = "!";
-            var noBackPlease = function() {
-                global.location.href += "#";
-
-                // making sure we have the fruit available for juice....
-                // 50 milliseconds for just once do not cost much (^__^)
-                global.setTimeout(function() {
-                    global.location.href += "!";
-                }, 50);
-            };
-
-            // Earlier we had setInerval here....
-            global.onhashchange = function() {
-                if (global.location.hash !== _hash) {
-                    global.location.hash = _hash;
-                }
-            };
-
-            global.onload = function() {
-
-                noBackPlease();
-
-                // disables backspace on page except on input fields and textarea..
-                document.body.onkeydown = function(e) {
-                    var elm = e.target.nodeName.toLowerCase();
-                    if (e.which === 8 && (elm !== 'input' && elm !== 'textarea')) {
-                        e.preventDefault();
-                    }
-                    // stopping event bubbling up the DOM tree..
-                    e.stopPropagation();
-                };
-
-            };
-
-        })(window);
-
+        /*  $(document).ready(function() {
+             window.history.pushState(null, "", window.location.href);
+             window.onpopstate = function() {
+                 window.history.pushState(null, "", window.location.href);
+             };
+         }); */
         /* no back prevention code */
     </script>
 
