f3ceed3860067c59a57b3214b4fce7444c58aad9
My Name <sakshi707>
2022-06-29 11:35:23 +0530

    added file for responsive

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,3 +26,4 @@ minifier.minify('public/after_login/new_ui/css/tab-responsive.css');
 minifier.minify('public/after_login/new_ui/css/mobile-responsive.css');
 /********** current css **********/
 minifier.minify('public/after_login/current_ui/css/style.css');
+minifier.minify('public/after_login/current_ui/css/mobile.css');
