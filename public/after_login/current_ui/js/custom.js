6d664adc82676ed0324a12a6f7a19997f15a3a9b
Shubham <sakshi707>
2022-06-29 20:30:59 +0530

    dashboard

--- public/after_login/current_ui/js/custom.js
+++ public/after_login/current_ui/js/custom.js
@@ -1,3 +1,33 @@
 $(".notificationnew").click(function() {
     $(".notification-block_new").show()
 });
+
+
+$(document).ready(function() {
+    $(".tooltipmain>svg").click(function(event) {
+        alert('djjdj');
+        event.stopPropagation();
+        $(".tooltipmain").each(function() {
+            $(this).siblings("p").show();
+        });
+        $(".tooltipmain").siblings("p").hide();
+        // $(this).siblings("p").addClass('show');
+
+    });
+    $(".tooltipmain>span").click(function() {
+        $(this).parent("p").hide();
+    });
+});
+$(document).on('click', function(e) {
+    var card_opened = $('.tooltipclass').hasClass('show');
+    if (!$(e.target).closest('.tooltipclass').length && !$(e.target).is('.tooltipclass') && card_opened === true) {
+        $('.tooltipclass').hide();
+    }
+});
+
+
+// dashboard-circuler progress
+
+
+
+
