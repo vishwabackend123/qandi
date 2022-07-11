811b0368771832ffa6b82ab6d06671e9b2599b79
My Name <sakshi707>
2022-07-11 17:16:11 +0530

    html

--- resources/views/auth/export_overall_analytics.blade.php
+++ resources/views/auth/export_overall_analytics.blade.php
@@ -265,6 +265,21 @@
                     <h3 class="boxheading d-flex align-items-center" style="margin-bottom: 24px;">Time management </h3>
                     <div class="row graph_head_label" style="margin-bottom:42px;">
                         <div class="col-md-6">
+                            <h4>Correct/ Incorrect answers</h4>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="graph_color_label_anwser text-right">
+                                <span style="margin-right:25px;"><small></small> Correct Answers</span>
+                                <span><small></small> Incorrect Answers</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="my_dataviz"></div>
+                </div>
+                <div class="commonWhiteBox commonblockDash">
+                    <h3 class="boxheading d-flex align-items-center" style="margin-bottom: 24px;">Time management </h3>
+                    <div class="row graph_head_label" style="margin-bottom:42px;">
+                        <div class="col-md-6">
                             <h4>Time spent on each question <span>(in Last week)</span></h4>
                         </div>
                         <div class="col-md-6">
@@ -605,7 +620,71 @@ const myChart2 = new Chart(
 );
 /* progress Journy graph end */
 
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 20, left: 50},
+    width = auto - margin.left - margin.right,
+    height = auto - margin.top - margin.bottom;
 
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv", function(data) {
+
+  // List of subgroups = header of the csv files = soil condition here
+  var subgroups = data.columns.slice(1)
+
+  // List of groups = species here = value of the first column called group -> I show them on the X axis
+  var groups = d3.map(data, function(d){return(d.group)}).keys()
+
+  // Add X axis
+  var x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.2])
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 60])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // color palette = one color per subgroup
+  var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#e41a1c','#377eb8','#4daf4a'])
+
+  //stack the data? --> stack per subgroup
+  var stackedData = d3.stack()
+    .keys(subgroups)
+    (data)
+
+  // Show the bars
+  svg.append("g")
+    .selectAll("g")
+    // Enter in the stack data = loop key per key = group per group
+    .data(stackedData)
+    .enter().append("g")
+      .attr("fill", function(d) { return color(d.key); })
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d) { return x(d.data.group); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width",x.bandwidth())
+})
 
 
 </script>  
