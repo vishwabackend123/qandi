1222b6ff4003a5550c675154468263b76cbd5f87
My Name <sakshi707>
2022-07-11 16:37:46 +0530

    html

--- resources/views/auth/export_overall_analytics.blade.php
+++ resources/views/auth/export_overall_analytics.blade.php
@@ -205,7 +205,7 @@
                         </div>
                         <div class="col-md-3">
                             <div>
-                                <canvas id="subjectChart" style="height:100%;width:100%;"></canvas>
+                                <canvas id="progressChart" style="height:100%;width:100%;"></canvas>
                             </div>
                         </div>
                         <div class="col-md-4">
@@ -217,74 +217,97 @@
                     </div>
                 </div>
                 <div class="commonWhiteBox commonblockDash subject_score_card">
-                        <h3 class="boxheading d-flex align-items-center">Subject Score 
-                                <span class="tooltipmain ml-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none"><g opacity=".2" stroke="#234628" stroke-width="1.667" stroke-linecap="round" stroke-linejoin="round"> <path d="M10 18.833a8.333 8.333 0 1 0 0-16.667 8.333 8.333 0 0 0 0 16.667zM10 13.833V10.5M10 7.166h.009"/></g></svg>
-                                <p class="tooltipclass">
-                                <span ><img style="width:34px;" src="http://localhost/Uniq_web/public/after_login/new_ui/images/cross.png"></span>
-                                    This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
-                                </p>
-                            </span>
-                        </h3>
-                        <p class="dashSubtext mb-4">Negative marking for incorrect answers is considered</p>
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <h5 class="mb-0">Maths</h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="halfdoughnut">
-                                        <canvas id="subjectChart"></canvas>
-                                    </div>
-                                    <div class="color_labels ms-5">
-                                        <span class="d-block">Correct <b><small></small>32</b></span>
-                                        <span class="d-block mt-3 mb-3">Incorrect <b><small></small>4</b></span>
-                                        <span class="d-block">Not Attempted <b><small style="background-color: #e5eaee;"></small>4</b></span>
-                                    </div>
+                    <h3 class="boxheading d-flex align-items-center">Subject Score </h3>
+                    <p class="dashSubtext mb-4">Negative marking for incorrect answers is considered</p>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <h5 class="mb-0">Maths</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="halfdoughnut">
+                                    <canvas id="subjectChart"></canvas>
+                                </div>
+                                <div class="color_labels ms-5">
+                                    <span class="d-block">Correct <b><small></small>32</b></span>
+                                    <span class="d-block mt-3 mb-3">Incorrect <b><small></small>4</b></span>
+                                    <span class="d-block">Not Attempted <b><small style="background-color: #e5eaee;"></small>4</b></span>
                                 </div>
                             </div>
-                            <div class="col-md-6 mb-3">
-                                <h5 class="mb-0">Physics</h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="halfdoughnut">
-                                        <canvas id="subjectChart-1"></canvas>
-                                    </div>
-                                    <div class="color_labels ms-5">
-                                        <span class="d-block">Correct <b><small></small>32</b></span>
-                                        <span class="d-block mt-3 mb-3">Incorrect <b><small></small>4</b></span>
-                                        <span class="d-block">Not Attempted <b><small style="background-color: #e5eaee;"></small>4</b></span>
-                                    </div>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <h5 class="mb-0">Physics</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="halfdoughnut">
+                                    <canvas id="subjectChart-1"></canvas>
+                                </div>
+                                <div class="color_labels ms-5">
+                                    <span class="d-block">Correct <b><small></small>32</b></span>
+                                    <span class="d-block mt-3 mb-3">Incorrect <b><small></small>4</b></span>
+                                    <span class="d-block">Not Attempted <b><small style="background-color: #e5eaee;"></small>4</b></span>
                                 </div>
                             </div>
-                            <div class="col-md-6 mb-3">
-                                <h5 class="mb-0">Chemistry</h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="halfdoughnut">
-                                        <canvas id="subjectChart-2"></canvas>
-                                    </div>
-                                    <div class="color_labels ms-5">
-                                        <span class="d-block">Correct <b><small></small>32</b></span>
-                                        <span class="d-block mt-3 mb-3">Incorrect <b><small></small>4</b></span>
-                                        <span class="d-block">Not Attempted <b><small style="background-color: #e5eaee;"></small>4</b></span>
-                                    </div>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <h5 class="mb-0">Chemistry</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="halfdoughnut">
+                                    <canvas id="subjectChart-2"></canvas>
+                                </div>
+                                <div class="color_labels ms-5">
+                                    <span class="d-block">Correct <b><small></small>32</b></span>
+                                    <span class="d-block mt-3 mb-3">Incorrect <b><small></small>4</b></span>
+                                    <span class="d-block">Not Attempted <b><small style="background-color: #e5eaee;"></small>4</b></span>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="commonWhiteBox commonblockDash">
+                    <h3 class="boxheading d-flex align-items-center" style="margin-bottom: 24px;">Time management </h3>
+                    <div class="row graph_head_label" style="margin-bottom:42px;">
+                        <div class="col-md-6">
+                            <h4>Time spent on each question <span>(in Last week)</span></h4>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="graph_color_label_anwser text-right">
+                                <span style="margin-right:25px;"><small></small> Correct Answers</span>
+                                <span><small></small> Incorrect Answers</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress_journey_chart">
+                        <canvas id="progressJourny_graph"></canvas>
+                    </div>
+                </div>
+                <div class="commonWhiteBox commonblockDash graph_head_label">
+                    <h3 class="boxheading d-flex align-items-center" style="margin-bottom: 24px;">Accuracy Percentage <span>(in Last week)</span></h3>
+                    <div class="graph_color_label_anwser" style="margin:34px 0 18px;">
+                        <span style="margin-right:25px;"><small></small> Correct Answers</span>
+                        <span><small></small> Incorrect Answers</span>
+                    </div>
+                    <div class="progress_journey_chart">
+                        <canvas id="progressJourny_graph_2"></canvas>
+                    </div>
+                </div>
+                <div  class="visit_link text-center">
+                    <a href="javascript:void(0)">To know more visit : https://app.thomsondigital2021.com/export_analytics</a>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<script>  
-/********* halfdoughnut progress - start ************/
-const circuference = 180;
+<script> 
+/************* subject *******/
+const circuference = 260;
 const data = {
-  labels: ["Progress"],
+  labels: ["Correct", "Incorrect", "Not Attempted"],
   datasets: [
     {
-    //   label: "My First Dataset",
-      data: [180, 100],
+      label: "My First Dataset",
+      data: [200, 100, 80],
       backgroundColor: [
-        "#08d5a1",  
+        "#08d5a1",
+        "#fb7686",
         "#f2f4f7"
       ]
     }
@@ -321,7 +344,270 @@ const config = {
   }
 };
 const myCharted = new Chart("subjectChart", config)
+/*******subject-1**********/
+const circuference_1 = 260;
+const data_1 = {
+  labels: ["Correct", "Incorrect", "Not Attempted"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [200, 100, 80],
+      backgroundColor: [
+        "#08d5a1",
+        "#fb7686",
+        "#f2f4f7"
+      ]
+    }
+  ]
+};
+const config_1 = {
+  type: "doughnut",
+  data: data,
+  options: {   
+    reponsive: true,
+    maintainAspectRatio: false,
+    rotation: (circuference / 2) * -1,
+    circumference: circuference,
+    cutout: "60%",
+    borderWidth: 0,
+    borderRadius: function (context, options) {
+      const index = context.dataIndex;
+      let radius = {};
+      if (index == 0) {
+        radius.innerStart = 0;
+        radius.outerStart = 0;
+      }
+      if (index === context.dataset.data.length - 1) {
+        radius.innerEnd = 0;
+        radius.outerEnd = 0;
+      }
+      return radius;
+    },
+    plugins: {
+      title: false,
+      subtitle: false,
+      legend: false
+    },
+  }
+};
+const myCharted_1 = new Chart("subjectChart-1", config_1)
+
+/********Subject-2*********/
+const circuference_2 = 260;
+const data_2 = {
+  labels: ["Correct", "Incorrect", "Not Attempted"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [200, 100, 80],
+      backgroundColor: [
+        "#08d5a1",
+        "#fb7686",
+        "#f2f4f7"
+      ]
+    }
+  ]
+};
+const config_2 = {
+  type: "doughnut",
+  data: data,
+  options: {   
+    reponsive: true,
+    maintainAspectRatio: false,
+    rotation: (circuference / 2) * -1,
+    circumference: circuference,
+    cutout: "60%",
+    borderWidth: 0,
+    borderRadius: function (context, options) {
+      const index = context.dataIndex;
+      let radius = {};
+      if (index == 0) {
+        radius.innerStart = 0;
+        radius.outerStart = 0;
+      }
+      if (index === context.dataset.data.length - 1) {
+        radius.innerEnd = 0;
+        radius.outerEnd = 0;
+      }
+      return radius;
+    },
+    plugins: {
+      title: false,
+      subtitle: false,
+      legend: false
+    },
+  }
+};
+const myCharted_2 = new Chart("subjectChart-2", config_2)
+/********* halfdoughnut progress - start ************/
+// const circuference_3 = 180;
+// const data_3 = {
+//   labels: ["Progress"],
+//   datasets: [
+//     {
+//     //   label: "My First Dataset",
+//       data: [180, 100],
+//       backgroundColor: [
+//         "#ff0000",  
+//         "#ddd"
+//       ]
+//     }
+//   ]
+// };
+// const config_3 = {
+//   type: "doughnut",
+//   data: data,
+//   options: {   
+//     reponsive: true,
+//     maintainAspectRatio: false,
+//     rotation: (circuference / 2) * -1,
+//     circuference: circuference,
+//     cutout: "60%",
+//     borderWidth: 0,
+//     borderRadius: function (context, options) {
+//       const index = context.dataIndex;
+//       let radius = {};
+//       if (index == 0) {
+//         radius.innerStart = 0;
+//         radius.outerStart = 0;
+//       }
+//       if (index === context.dataset.data.length - 1) {
+//         radius.innerEnd = 0;
+//         radius.outerEnd = 0;
+//       }
+//       return radius;
+//     },
+//     plugins: {
+//       title: false,
+//       subtitle: false,
+//       legend: false
+//     },
+//   }
+// };
+// const myCharted_3 = new Chart("progressChart", config_3)
 /*********** progress end ****************/
+
+ /* progress Journy graph */
+const labels1 = ['13 May','14 May','15 May','16 May','17 May','18 May','19 May','20 May'];
+const data1 = {
+    labels: labels1,
+    datasets: [{
+            label: 'Ideal Pace',
+            backgroundColor: '#7db9ff',
+            borderColor: '#7db9ff',
+            data:"[0, 10, 5, 2, 20, 30, 45]",
+            borderwidth: 0.6,
+            tension: 0.4
+        },
+        {
+            label: 'Your Pace',
+            backgroundColor: '#56b663',
+            borderColor: '#56b663',
+            data: "[0, 5, 15, 2, 23, 30, 25]",
+            borderwidth: 0.6,
+            tension: 0.4
+        }
+    ]
+};
+const config1 = {
+    type: 'line',
+    data: data1,
+    options: {
+        responsive: true,
+        elements: {
+            point: {
+                radius: 0
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: false,
+                text: 'Chart.js Line Chart - Cubic interpolation mode'
+            },
+        },
+        interaction: {
+            intersect: false,
+        },
+        scales: {
+            x: {
+                grid: {
+                    display: false
+                }
+            }
+
+        }
+    }
+};
+const myChart1 = new Chart(
+    document.getElementById('progressJourny_graph'),
+    config1
+);
+
+const labels2 = ['13 May','14 May','15 May','16 May','17 May','18 May','19 May','20 May'];
+const data2 = {
+    labels: labels2,
+    datasets: [{
+            label: 'Ideal Pace',
+            backgroundColor: '#7db9ff',
+            borderColor: '#7db9ff',
+            data:"[0, 10, 5, 2, 20, 30, 45]",
+            borderwidth: 0.6,
+            tension: 0.4
+        },
+        {
+            label: 'Your Pace',
+            backgroundColor: '#56b663',
+            borderColor: '#56b663',
+            data: "[0, 5, 15, 2, 23, 30, 25]",
+            borderwidth: 0.6,
+            tension: 0.4
+        }
+    ]
+};
+const config2 = {
+    type: 'line',
+    data: data2,
+    options: {
+        responsive: true,
+        elements: {
+            point: {
+                radius: 0
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: false,
+                text: 'Chart.js Line Chart - Cubic interpolation mode'
+            },
+        },
+        interaction: {
+            intersect: false,
+        },
+        scales: {
+            x: {
+                grid: {
+                    display: false
+                }
+            }
+
+        }
+    }
+};
+const myChart2 = new Chart(
+    document.getElementById('progressJourny_graph_2'),
+    config1
+);
+/* progress Journy graph end */
+
+
+
+
 </script>  
 
 @endsection
