bcb2e53bb1e76e01461b03b3bd44671efd8e7bd4
sachin <sachinawasthi1989@gmail.com>
2022-07-12 14:14:11 +0530

    exam_screen

--- resources/views/auth/exam_test.blade.php
+++ resources/views/auth/exam_test.blade.php
@@ -6,13 +6,11 @@
 
 <div class="exam-wrapper">
 <div class="slider">
-
-<div class="owl-carousel owl-theme">
-<div class="item"><h4>1</h4></div>
-<div class="item"><h4>2</h4></div>
-<div class="item"><h4>3</h4></div>
-
-</div>
+    <div class="owl-carousel owl-theme">
+        <div class="item"><h4>1</h4></div>
+        <div class="item"><h4>2</h4></div>
+        <div class="item"><h4>3</h4></div>
+    </div>
 </div>
 
 
