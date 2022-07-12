15666883f591c60fbaa91159dba3c26d204d4f8c
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-07-12 19:32:22 +0530

    containt issue

--- resources/views/afterlogin/dashboard.blade.php
+++ resources/views/afterlogin/dashboard.blade.php
@@ -38,7 +38,7 @@ $userData = Session::get('user_data');
                                     </svg>
                                     <p class="tooltipclass">
                                         <span><img style="width:34px;" src="http://localhost/Uniq_web/public/after_login/new_ui/images/cross.png"></span>
-                                        This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
+                                        A score derived from the detailed analysis of your test patterns that gives a clear understanding of your current level of preparation in comparison to an ideal one. Measure your real-time probability of reaching the goal with your current pattern of preparation. Set your goal!
                                     </p>
                                 </span>
                             </h3>
@@ -148,7 +148,7 @@ $userData = Session::get('user_data');
                                         </svg>
                                         <p class="tooltipclass">
                                             <span><img style="width:34px;" src="http://localhost/Uniq_web/public/after_login/new_ui/images/cross.png"></span>
-                                            This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
+                                            A list of customized tasks specially personalized for you based on the in-depth analysis of your completed tests. Strengthen your core learning and strategic skills through these quick customized tests. Build on your strengths and work on your weaker areas to progressively improve them. Improve on your proficiency!
                                         </p>
                                     </span>
                                 </h3>
@@ -344,7 +344,7 @@ $userData = Session::get('user_data');
                                         </svg>
                                         <p class="tooltipclass">
                                             <span><img style="width:34px;" src="http://localhost/Uniq_web/public/after_login/new_ui/images/cross.png"></span>
-                                            This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
+                                            A matrix created to analyse your attempts in various topics over time and sort them into your areas of strengths and weaknesses.  This data will keep on changing as you progress and diligently work on your identified and analysed weaknesses and strengths. It will also make visible those topics that can become your strength with a little more effort on your part. Align your preparation now!
                                         </p>
                                     </span>
                                 </h3>
@@ -484,7 +484,7 @@ $userData = Session::get('user_data');
                                                         </svg>
                                                         <p class="tooltipclass">
                                                             <span><img style="width:34px;" src="http://localhost/Uniq_web/public/after_login/new_ui/images/cross.png"></span>
-                                                            This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
+                                                            To reduce uncertainty and increase your efficiency and chances of success, it is absolutely essential that you plan your preparation with great care. With effective planning comes motivation, productivity, satisfaction, and ultimately success. Go ahead and plan your week!
                                                         </p>
                                                     </span>
                                                 </h3>
@@ -691,7 +691,7 @@ $userData = Session::get('user_data');
                                                     </svg>
                                                     <p class="tooltipclass">
                                                         <span><img style="width:34px;" src="http://localhost/Uniq_web/public/after_login/new_ui/images/cross.png"></span>
-                                                        This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
+                                                        Mapping your progress journey against an ideal path lets you draw valuable insights about the rate at which you are progressing with respect to the ideal path that will lead you to success. It will help you judge whether you are keeping pace or lagging behind, for you to take corrective action. Pick up your pace!
                                                     </p>
                                                 </span>
                                             </h3>
@@ -753,7 +753,7 @@ $userData = Session::get('user_data');
                                                     </svg>
                                                     <p class="tooltipclass">
                                                         <span><img style="width:34px;" src="http://localhost/Uniq_web/public/after_login/new_ui/images/cross.png"></span>
-                                                        This card represents a combination of your skill, expertise, and knowledge in the topics you have attempted. Build your proficiencies!
+                                                        This chart will give insights and a deep understanding of your ongoing preparation, and your improvement over time. An increasing trend is what you should ideally be maintaining. Go uptrend!
                                                     </p>
                                                 </span>
                                             </h3>
