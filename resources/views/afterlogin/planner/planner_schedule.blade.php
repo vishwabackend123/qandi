cfc91c75c02a77ab26ba61a23916650e4d999a1e
dinesh@ahom.tech-at-225208115870 <dinesh@ahom.tech-at-225208115870>
2022-07-15 18:44:32 +0530

    planner blade

--- resources/views/afterlogin/planner/planner_schedule.blade.php
+++ resources/views/afterlogin/planner/planner_schedule.blade.php
@@ -166,6 +166,7 @@ $userData = Session::get('user_data');
     </div>
 </div>
 <!-- Footer Section -->
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js" integrity="sha512-UdIMMlVx0HEynClOIFSyOrPggomfhBKJE28LKl8yR3ghkgugPnG6iLfRfHwushZl1MOPSY6TsuBDGPK2X4zYKg==" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js" integrity="sha512-6Uv+497AWTmj/6V14BsQioPrm3kgwmK9HYIyWP+vClykX52b0zrDGP7lajZoIY1nNlX4oQuh7zsGjmF7D0VZYA==" crossorigin="anonymous"></script>
 
@@ -426,8 +427,10 @@ $userData = Session::get('user_data');
         var subject_id = $(this).attr('value');
 
         var selected_count = $('#planner_sub_' + subject_id + ' div').length;
+        $("#number").val(parseInt($("#number").val()) - 1);
 
         var limitrange = $("#number").val();
+
         var chapterscount = $('input[name="chapters[]"]').length;
         if (limitrange == '0') {
             $('#saveplannerbutton').addClass('disabled');
