032fa14d5f57beba352d7d52ff645c884c9c09c7
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-07-20 11:39:36 +0530

    overall issue

--- resources/views/afterlogin/layouts/sidebar_new.blade.php
+++ resources/views/afterlogin/layouts/sidebar_new.blade.php
@@ -77,7 +77,7 @@
             </a>
         </li>
         <li class="mb-4">
-            <a href="javascript:void(0)">
+            <a href="{{route('overall_analytics')}}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M21.21 15.89A10 10 0 1 1 8 2.83" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M22 12A10 10 0 0 0 12 2v10h10z" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
