2aacf927d675b53ae43d1f3f03d6454b148f1fb2
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-07-20 12:24:23 +0530

    chapter page issue

--- resources/views/afterlogin/layouts/sidebar_new.blade.php
+++ resources/views/afterlogin/layouts/sidebar_new.blade.php
@@ -52,7 +52,7 @@
         <span class="custom-border mt-3"></span>
     </span>
     <ul class="sidebar-menu-lists">
-        <li class="active mb-4">
+        <li class="active mb-4 current_dashboard">
             <a href="{{ url('/dashboard') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
                     <path d="M25.667 9.94V4.643c0-1.645-.747-2.31-2.602-2.31h-4.713c-1.855 0-2.602.665-2.602 2.31v5.285c0 1.657.747 2.31 2.602 2.31h4.713c1.855.012 2.602-.653 2.602-2.298zM25.667 23.065v-4.713c0-1.855-.747-2.602-2.602-2.602h-4.713c-1.855 0-2.602.747-2.602 2.602v4.713c0 1.855.747 2.602 2.602 2.602h4.713c1.855 0 2.602-.747 2.602-2.602zM12.25 9.94V4.643c0-1.645-.746-2.31-2.601-2.31H4.934c-1.855 0-2.601.665-2.601 2.31v5.285c0 1.657.746 2.31 2.601 2.31H9.65c1.855.012 2.601-.653 2.601-2.298zM12.25 23.065v-4.713c0-1.855-.746-2.602-2.601-2.602H4.934c-1.855 0-2.601.747-2.601 2.602v4.713c0 1.855.746 2.602 2.601 2.602H9.65c1.855 0 2.601-.747 2.601-2.602z" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -63,7 +63,7 @@
                 <span class="sidebar-menu-tooltip">Dashboard</span>
             </a>
         </li>
-        <li class="mb-4 sidebar-exam-menu">
+        <li class="mb-4 sidebar-exam-menu current_practice">
             <a href="javascript:void(0)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
                     <path d="M15.471 4.2 5.893 14.34c-.362.385-.712 1.143-.782 1.668l-.431 3.78c-.152 1.365.828 2.298 2.181 2.065l3.757-.642c.525-.093 1.26-.478 1.621-.875l9.579-10.138c1.656-1.75 2.403-3.745-.175-6.183-2.567-2.415-4.515-1.564-6.172.186z" stroke="#234628" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
@@ -76,7 +76,7 @@
                 <span class="sidebar-menu-tooltip">Practice Test</span>
             </a>
         </li>
-        <li class="mb-4">
+        <li class="mb-4 current_analytics">
             <a href="{{route('overall_analytics')}}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M21.21 15.89A10 10 0 1 1 8 2.83" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -89,7 +89,7 @@
                 <span class="sidebar-menu-tooltip">Test Analytics</span>
             </a>
         </li>
-        <li class="mb-4">
+        <li class="mb-4 current_planner">
             <a href="{{ url('/planner') }}">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M19 4H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zM16 2v4M8 2v4M3 10h18" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -286,6 +286,13 @@
 <!-- Test Summary Modal -->
 
 <script>
+    var url = window.location.pathname.split("/");
+    var action_method = url[2];
+    if(action_method == 'overall_analytics')
+    {
+        $('.current_dashboard').removeClass('active');
+        $('.current_analytics').addClass('active');
+    }
     function stateHandle() {
         if (document.querySelector(".emaillink").value === "") {
             //button.disabled = true;
