14c027740de6f859c9c1e19eb93b9c05bdcf3725
dinesh@ahom.tech-at-225208115870 <dinesh@ahom.tech-at-225208115870>
2022-07-28 18:40:51 +0530

    Issue 2177 resolved

--- resources/views/afterlogin/AdaptiveExamTopic/adaptiveExam.blade.php
+++ resources/views/afterlogin/AdaptiveExamTopic/adaptiveExam.blade.php
@@ -52,13 +52,11 @@ $question_type = "Numerical";
                             <div class="examTabheader">
                                 <div class="tablist">
                                     <ul class="nav nav-tabs" role="tablist" id="myTab">
-                                        @if(!empty($filtered_subject))
-                                        @foreach($filtered_subject as $key=>$sub)
+
                                         <li class="nav-item">
-                                            <a class="nav-link qq1_2_3_4 all_div class_{{$sub->id}} @if($activesub_id==$sub->id) active @endif " id="{{$sub->subject_name}}-tab" data-bs-toggle="tab" href="#{{$sub->subject_name}}" @if(count($filtered_subject)>1) onclick="get_subject_question('{{$sub->id}}')" @endif >{{$sub->subject_name}} </a>
+                                            <a class="nav-link qq1_2_3_4 all_div  active  " id="adaptive-tab" data-bs-toggle="tab" href="#adaptive_exam" role="tab" aria-controls="adaptive" aria-selected="true">Adaptive Exam</a>
                                         </li>
-                                        @endforeach
-                                        @endif
+
                                     </ul>
                                 </div>
                                 <div class="submitBtn">
