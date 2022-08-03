e25258ea9e3083db001caa8c82967d794b43fea0
Shubham <sakshi707>
2022-08-03 15:36:32 +0530

    custom exam

--- resources/views/afterlogin/ExamCustom/exam_custom.blade.php
+++ resources/views/afterlogin/ExamCustom/exam_custom.blade.php
@@ -56,7 +56,7 @@
                                 </div>
                                 @isset($subject_list)
                                 @foreach($subject_list as $skey=>$sub)
-                                <div class="take-fulltest d-lg-flex align-items-center justify-content-end {{($skey==0)?'d-lg-flex':'d-none'}}" id="{{$sub->subject_name}}_main">
+                                <div class="take-fulltest d-lg-flex2 align-items-center justify-content-end {{($skey==0)?'d-lg-flex2':'d-none2'}}" id="{{$sub->subject_name}}_main">
                                     <div class="d-sm-flex align-items-center clrsec topic_form" id="{{$sub->subject_name}}_select">
                                         <form id="topic_form" method="post" action="{{route('custom_exam_topic','instruction')}}" class="topic_list_form text-sm-right">
                                             @csrf
@@ -157,7 +157,7 @@
         $('#' + subject + '_test').addClass('d-flex');
         $('.topic_form').attr("style", "display: none  !important");
         $('.take-fulltest').attr("style", "display: none  !important");
-        $('#' + subject + '_main').attr("style", "display: flex !important");
+        $('#' + subject + '_main').attr("style", "display: flex");
         let is_select=false;
         $(".topic_" + subject).each(function() {
         
@@ -168,7 +168,7 @@
        });
         if(is_select)
         {
-            $('#' + subject + '_select').attr("style", "display: flex !important");
+            $('#' + subject + '_select').attr("style", "display: flex");
         }
         
         //clearTopics();
