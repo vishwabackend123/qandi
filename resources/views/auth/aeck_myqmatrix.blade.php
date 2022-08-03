4de4c6869b7e0ea381b666eb6134324bfddc2cb9
Shubham <sakshi707>
2022-08-03 11:13:56 +0530

    aeck

--- resources/views/auth/aeck_myqmatrix.blade.php
+++ resources/views/auth/aeck_myqmatrix.blade.php
@@ -187,7 +187,7 @@
                     <div class="commonWhiteBox">
                         <div class="tabMainblock">
                             <div class="commontab aeck_commontab">
-                                <div class="tablist">
+                                <div class="tablist mobilescrolltab">
                                     <ul class="nav nav-tabs" role="tablist">
                                         <li class="nav-item">
                                             <a class="nav-link qq1_2_3_4 active" data-bs-toggle="tab" href="#evaluation">Evaluation</a>
