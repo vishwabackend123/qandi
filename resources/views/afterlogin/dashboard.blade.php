a1ac628b7755c0e5fbc12c5120b18d075358e591
Shubham <sakshi707>
2022-08-12 11:01:43 +0530

    content

--- resources/views/afterlogin/dashboard.blade.php
+++ resources/views/afterlogin/dashboard.blade.php
@@ -190,7 +190,7 @@ $user_id = isset($userData->id)?$userData->id:'';
                             <div class="fullbodyBox">
                                 <div class="leftBox">
                                     <h4>Full body scan test</h4>
-                                    <p>to assess yourpreparation and take your first step to improvement.</p>
+                                    <p>to assess your preparation. Take your first step to improvement.</p>
                                     <a href="{{route('exam',['full_exam','instruction'])}}" class="btn btn-common-white">Attempt Now</a>
                                 </div>
                                 <div class="rightImgBox">
