80657a0af20ee1d4b85c359ca4d874dc8664f9da
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-08-19 19:02:20 +0530

    rate limit

--- app/Http/Controllers/StudentSignInController.php
+++ app/Http/Controllers/StudentSignInController.php
@@ -367,6 +367,16 @@ class StudentSignInController extends Controller
             curl_close($curl);
 
             $aResponse = json_decode($response_json);
+            if($httpcode == 503)
+            {
+                $errmsg = isset($aResponse->message) ? $aResponse->message : "OTP retry limit exceeded. Try again in 24 hours.";
+
+                $response = [
+
+                    "error" => $err, "msg" => $errmsg, "success" => false, "status" => 400,
+                ];
+                 return json_encode($response);
+            }
 
             $success = isset($aResponse->success) ? $aResponse->success : false;
 
