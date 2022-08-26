91a93d641d3eaad50e888e62774c15bd9c867d87
Shubham <sakshi707>
2022-08-26 17:57:38 +0530

    datepicker

--- resources/views/afterlogin/planner/planner_schedule.blade.php
+++ resources/views/afterlogin/planner/planner_schedule.blade.php
@@ -50,7 +50,7 @@ $userData = Session::get('user_data');
                                     <div class="planner-date position-relative">
                                         <label>End Date</label>
 
-                                        <input type="date" class="form-control plannerDate" id="EndDate" name="end_date" value="{{$sundayDate}}" onkeydown="return false" readonly>
+                                        <input type="date" class="form-control plannerDate plannerEndDate" id="EndDate" name="end_date" value="{{$sundayDate}}" onkeydown="return false" readonly>
                                         <span class="position-absolute clander-icon"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M15.833 3.333H4.167C3.247 3.333 2.5 4.08 2.5 5v11.667c0 .92.746 1.666 1.667 1.666h11.666c.92 0 1.667-.746 1.667-1.666V5c0-.92-.746-1.667-1.667-1.667zM13.333 1.667V5M6.667 1.667V5M2.5 8.333h15" stroke="#CCC" stroke-width="1.667" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg></span>
