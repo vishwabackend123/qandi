ad1f390ac7af8e261a903de094d0a70ff0a85e27
My Name <sakshi707>
2022-08-31 10:14:41 +0530

    500 page html

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,8 +7,8 @@
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     @if(env('MINIFY_STATIC_ASSETS') == 'true')
-    <link rel="stylesheet" href="{{URL::asset('public/after_login/current_ui/css/style.min.css')}}">
-    <link rel="stylesheet" href="{{URL::asset('public/after_login/current_ui/css/mobile.min.css')}}">
+    <link rel="stylesheet" href="https://app.thomsondigital2021.com/public/after_login/current_ui/css/style.min.css">
+    <link rel="stylesheet" href="https://app.thomsondigital2021.com/public/after_login/current_ui/css/mobile.min.css')}}">
     @else
     <link rel="stylesheet" href="{{URL::asset('public/after_login/current_ui/css/style.css')}}">
     <link rel="stylesheet" href="{{URL::asset('public/after_login/current_ui/css/mobile.css')}}">
