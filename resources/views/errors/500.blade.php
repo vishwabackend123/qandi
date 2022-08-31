b49c139763a8e75a58d5409436eb56f1d5c135ac
My Name <sakshi707>
2022-08-31 11:08:03 +0530

    500 page html

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -86,7 +86,7 @@
                 <h2>Internal Server Error</h2>
                 <p>We are having some issues at the moment. 
                     We will have it fixed in no time!</p>
-                <a href="javascript:void(0)" id="reloadPage" class="btn btn-common-transparent">Reload 
+                <a  id="reloadPage" class="btn btn-common-transparent">Reload 
                     <svg style="margin-left:8px;" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M16.562 7.084h-3.75a.833.833 0 0 1 0-1.667h2.917V2.5a.833.833 0 1 1 1.667 0v3.75a.833.833 0 0 1-.834.834z" fill="#56B663"/>
                         <path d="M10 18.334a8.334 8.334 0 1 1 7.21-12.501.833.833 0 0 1-1.442.835A6.658 6.658 0 1 0 16.667 10a.833.833 0 0 1 1.667 0A8.343 8.343 0 0 1 10 18.334z" fill="#56B663"/>
@@ -96,6 +96,7 @@
         </div>
     </div>
 
+    <script type="text/javascript" src="{{URL::asset('public/js/jquery-3.6.0.min.js')}}"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             $("#reloadPage").click(function () {
