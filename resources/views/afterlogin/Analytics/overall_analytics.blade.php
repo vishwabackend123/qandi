5d2d5e38bdb5fb63c3dee46a5c3f231b9eceb127
My Name <sakshi707>
2022-09-05 10:33:22 +0530

    UI issues

--- resources/views/afterlogin/Analytics/overall_analytics.blade.php
+++ resources/views/afterlogin/Analytics/overall_analytics.blade.php
@@ -1228,4 +1228,37 @@ function showProgress(exam_type,value) {
             color: #1f1f1f;
         }
     </style>
+    <script>
+        $.fn.tabbing = function (options) {
+    var opts = {delayTime : 300};
+    options = options || {};
+    opts = $.extend(opts,options);    
+    return this.each(function () {
+        $(this).on('click', function (event) {
+            event.preventDefault();
+            var sum = 0;
+            $(this).prevAll().each(function(){  sum += $(this).width();});
+          var get = document.getElementById('tabs').scrollWidth
+            var dist = sum - ( $(this).parent().width() - $(this).width()) / 2;
+          if(dist < 0){
+            dist = 0;
+          }
+          /* else if(dist+sum > get){
+            dist = get-sum+dist+dist;
+          } */
+            $(this).parent().animate({
+                scrollLeft: dist
+            },opts['delayTime']);
+        });
+    });
+};
+$('#tabs li').tabbing();
+
+
+$('#tabs li').click(function(){
+  var  hashit = $(this).find('a').attr('href')
+  var autoHeight = $(hashit).height() + 30;
+$('.tab-content').animate({height: autoHeight}, 100);
+});
+    </script>
 @endsection
