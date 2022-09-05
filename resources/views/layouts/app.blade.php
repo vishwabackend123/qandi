0736f83f111bb602822e67f143c2cee7126c931f
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-09-05 16:25:45 +0530

    try back button issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,12 @@
             null
         };
     </script>
+    <script type="text/javascript">
+        window.history.forward();
+        function noBack() {
+            window.history.forward();
+        }
+    </script>
 </head>
 
 <body>
