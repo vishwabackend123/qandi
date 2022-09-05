ef79cfe02267930c8b925a4ab5549b5d52a9b0e0
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-09-05 18:14:04 +0530

    catch issue

--- resources/views/afterlogin/layouts/app_new.blade.php
+++ resources/views/afterlogin/layouts/app_new.blade.php
@@ -9,6 +9,9 @@
     <title>Q&I</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap" rel="stylesheet">
     <link rel="icon" href="{{URL::asset('public/images/favicon.ico')}}" type="{{URL::asset('public/image/x-icon')}}" />
