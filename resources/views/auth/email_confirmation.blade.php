9b8ed4c7c082dd7f114628f3d4388c6a970dfa7b
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-09-07 15:39:38 +0530

    logo click

--- resources/views/auth/email_confirmation.blade.php
+++ resources/views/auth/email_confirmation.blade.php
@@ -2,29 +2,31 @@
 @section('content')
 <div class="plan_successfull_wrapper">
     <div class="plan_successfull_heder_icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="66" viewBox="0 0 64 66" fill="none" class="plan_successfull_heder_icon_img">
-            <g filter="url(#67epj5et9a)">
-                <path d="M32 6H8v24h24V6zM55.993 30.002H31.994V54h24V30z" fill="#38D430" />
-                <path d="M55.993 6H31.994v24h24V6z" fill="#00AB16" />
-                <path d="m46.775 23.281 2.018 2.08h2.33l-3.245-3.36 2.08-2.413V17.18l-3.183 3.68-3.057-3.162.894-.64c1.04-.77 2.08-1.788 2.08-3.244 0-2.101-1.6-3.495-3.785-3.495-1.893 0-3.744 1.352-3.744 3.599 0 1.372 1.019 2.475 1.705 3.2l.437.457-.291.208c-1.56 1.144-2.58 2.24-2.58 4.098 0 1.892 1.457 3.766 4.16 3.766 1.664 0 2.933-.938 4.181-2.365zM40.91 13.81a1.958 1.958 0 0 1 1.997-1.997c1.227 0 2.038.873 2.038 1.955 0 .728-.312 1.352-1.206 1.997l-1.103.832-.582-.624c-.562-.562-1.144-1.31-1.144-2.164zm-.728 7.924c0-1.144.665-1.872 1.477-2.475l.728-.562 3.307 3.432-.125.146c-.853.977-1.789 1.747-2.954 1.747-1.518 0-2.433-1.144-2.433-2.288zM27.64 18.001a7.653 7.653 0 1 0-7.667 7.68 7.452 7.452 0 0 0 4.374-1.472l1.259 1.264h3.638l-3.075-3.088a7.5 7.5 0 0 0 1.47-4.384zm-3.453 2.4-2.093-2.099h-3.636l3.92 3.936a4.9 4.9 0 1 1 1.81-1.837zm15.26 14.24h9.12v2.699h-3.123v9.323h3.11v2.697h-9.12v-2.694h3.13v-9.323h-3.117v-2.702z" fill="#1F1F1F" />
-            </g>
-            <defs>
-                <filter id="67epj5et9a" x="0" y="2" width="63.993" height="64.001" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                    <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                    <feOffset dy="4" />
-                    <feGaussianBlur stdDeviation="4" />
-                    <feColorMatrix values="0 0 0 0 0.641667 0 0 0 0 0.67375 0 0 0 0 0.7 0 0 0 0.1 0" />
-                    <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_532_2530" />
-                    <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                    <feOffset dy="4" />
-                    <feGaussianBlur stdDeviation="4" />
-                    <feColorMatrix values="0 0 0 0 0.641667 0 0 0 0 0.67375 0 0 0 0 0.7 0 0 0 0.1 0" />
-                    <feBlend in2="effect1_dropShadow_532_2530" result="effect2_dropShadow_532_2530" />
-                    <feBlend in="SourceGraphic" in2="effect2_dropShadow_532_2530" result="shape" />
-                </filter>
-            </defs>
-        </svg>
+        <a href="{{env('CMS_URL')}}" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="64" height="66" viewBox="0 0 64 66" fill="none" class="plan_successfull_heder_icon_img">
+                <g filter="url(#67epj5et9a)">
+                    <path d="M32 6H8v24h24V6zM55.993 30.002H31.994V54h24V30z" fill="#38D430" />
+                    <path d="M55.993 6H31.994v24h24V6z" fill="#00AB16" />
+                    <path d="m46.775 23.281 2.018 2.08h2.33l-3.245-3.36 2.08-2.413V17.18l-3.183 3.68-3.057-3.162.894-.64c1.04-.77 2.08-1.788 2.08-3.244 0-2.101-1.6-3.495-3.785-3.495-1.893 0-3.744 1.352-3.744 3.599 0 1.372 1.019 2.475 1.705 3.2l.437.457-.291.208c-1.56 1.144-2.58 2.24-2.58 4.098 0 1.892 1.457 3.766 4.16 3.766 1.664 0 2.933-.938 4.181-2.365zM40.91 13.81a1.958 1.958 0 0 1 1.997-1.997c1.227 0 2.038.873 2.038 1.955 0 .728-.312 1.352-1.206 1.997l-1.103.832-.582-.624c-.562-.562-1.144-1.31-1.144-2.164zm-.728 7.924c0-1.144.665-1.872 1.477-2.475l.728-.562 3.307 3.432-.125.146c-.853.977-1.789 1.747-2.954 1.747-1.518 0-2.433-1.144-2.433-2.288zM27.64 18.001a7.653 7.653 0 1 0-7.667 7.68 7.452 7.452 0 0 0 4.374-1.472l1.259 1.264h3.638l-3.075-3.088a7.5 7.5 0 0 0 1.47-4.384zm-3.453 2.4-2.093-2.099h-3.636l3.92 3.936a4.9 4.9 0 1 1 1.81-1.837zm15.26 14.24h9.12v2.699h-3.123v9.323h3.11v2.697h-9.12v-2.694h3.13v-9.323h-3.117v-2.702z" fill="#1F1F1F" />
+                </g>
+                <defs>
+                    <filter id="67epj5et9a" x="0" y="2" width="63.993" height="64.001" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                        <feOffset dy="4" />
+                        <feGaussianBlur stdDeviation="4" />
+                        <feColorMatrix values="0 0 0 0 0.641667 0 0 0 0 0.67375 0 0 0 0 0.7 0 0 0 0.1 0" />
+                        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_532_2530" />
+                        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                        <feOffset dy="4" />
+                        <feGaussianBlur stdDeviation="4" />
+                        <feColorMatrix values="0 0 0 0 0.641667 0 0 0 0 0.67375 0 0 0 0 0.7 0 0 0 0.1 0" />
+                        <feBlend in2="effect1_dropShadow_532_2530" result="effect2_dropShadow_532_2530" />
+                        <feBlend in="SourceGraphic" in2="effect2_dropShadow_532_2530" result="shape" />
+                    </filter>
+                </defs>
+            </svg>
+        </a>
     </div>
     <div class="email-confirmation token_verfiy">
         <div class="email-profile-pick">
@@ -73,7 +75,7 @@
                 <path d="M58.834 64.167H42.166c-5 0-8.334-2.5-8.334-8.334V44.168c0-5.834 3.334-8.334 8.334-8.334h16.666c5 0 8.334 2.5 8.334 8.334v11.666c0 5.834-3.334 8.334-8.334 8.334z" stroke="#039855" stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                 <path d="m58.833 45-5.216 4.167c-1.717 1.366-4.534 1.366-6.25 0L42.166 45" stroke="#039855" stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
-        </div>        
+        </div>
         <p class="email_head"><span>Email link Sent</span></p>
         <p><label><b></b> A verification link has been sent to <strong>{{$response_json['email']}}</strong>, please click the link to get your account verified</label></p>
         <div class="d-flex align-items-center justify-content-center">
@@ -83,7 +85,8 @@
     @endif
 </div>
 <script type="text/javascript">
-    $('.email_send').hide();
+$('.email_send').hide();
+
 </script>
 @if(isset($response_json['student_id']))
 <script type="text/javascript">
