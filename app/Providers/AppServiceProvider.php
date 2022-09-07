6bc8f42162a30672c5e24a78f8955c139baec5d0
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-09-07 12:12:44 +0530

    reverse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,5 @@ class AppServiceProvider extends ServiceProvider
         if (env('FORCE_SCHEME_HTTPS')) {
             // URL::forceScheme('https');
         }
-         URL::forceScheme('https');
     }
 }
