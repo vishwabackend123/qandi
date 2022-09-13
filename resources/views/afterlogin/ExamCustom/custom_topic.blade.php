2cf813a87f35b3e99a8ad804509c673d79ab9725
Shubham <sakshi707>
2022-09-13 12:10:41 +0530

    custom exam

--- resources/views/afterlogin/ExamCustom/custom_topic.blade.php
+++ resources/views/afterlogin/ExamCustom/custom_topic.blade.php
@@ -4,23 +4,6 @@
     <div class="exam-box">
         <div class="exambox-heading d-flex align-items-center justify-content-between pb-3">
             <p title="{{$topic->topic_name}}">{{$topic->topic_name}}</p>
-            
-        </div>
-        <!-- <div class="topic_score_bar mb-3">
-            <div class="progress">
-                <div class="progress-bar examE" role="progressbar" style="width: {{$topic->E_ques_attempted}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                <div class="progress-bar examA" role="progressbar" style="width:{{$topic->A_ques_attempted}} %" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                <div class="progress-bar examC" role="progressbar" style="width: {{$topic->C_ques_attempted}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                <div class="progress-bar examK" role="progressbar" style="width: {{$topic->K_ques_attempted}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-        </div> -->
-        <div class="exam-cate d-flex align-items-center justify-content-between">
-            <!-- <div class="d-flex align-items-center">
-                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">E</a>
-                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">A</a>
-                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">C</a>
-                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">K</a>
-            </div> -->
             <h2>Proficiency: <span>
                     @if(isset($topic->topic_score))
                     {{round($topic->topic_score)}}%
@@ -29,7 +12,22 @@
                     @endif
                 </span>
             </h2>
-            <div class="circulergraph"></div>
+        </div>
+        <div class="topic_score_bar mb-3">
+            <div class="progress">
+                <div class="progress-bar examE" role="progressbar" style="width: {{$topic->E_ques_attempted}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                <div class="progress-bar examA" role="progressbar" style="width:{{$topic->A_ques_attempted}} %" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                <div class="progress-bar examC" role="progressbar" style="width: {{$topic->C_ques_attempted}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                <div class="progress-bar examK" role="progressbar" style="width: {{$topic->K_ques_attempted}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+        <div class="exam-cate d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center">
+                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">E</a>
+                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">A</a>
+                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">C</a>
+                <a href="javascript:void(0)" style="cursor:default;pointer-events: none;">K</a>
+            </div>
             <button id="chpt_topic_{{$topic->id}}" onclick="addOrRemove('{{$topic->id}}')" class="btn btn-common-transparent bg-transparent addremovetopic topic_{{$subject_name}}">Select</button>
         </div>
     </div>
