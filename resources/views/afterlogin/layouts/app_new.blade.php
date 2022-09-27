0b919def35decc37359b38673c0d46d8810f4ec5
Shubham <sakshi707>
2022-09-27 15:51:05 +0530

    review exam

--- resources/views/afterlogin/layouts/app_new.blade.php
+++ resources/views/afterlogin/layouts/app_new.blade.php
@@ -8,8 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Q&I</title>
     <meta name="description" content="">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    <meta name="viewport" content="width=device-width;initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Cache-Control" content="no-cache">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
