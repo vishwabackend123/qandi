c6c203b546c01d78226a2a0c9cdde2b9d924ed3f
My Name <sakshi707>
2022-10-03 21:24:56 +0530

    Ui fixes

--- resources/views/afterlogin/dashboard.blade.php
+++ resources/views/afterlogin/dashboard.blade.php
@@ -75,9 +75,9 @@ $user_id = isset($userData->id)?$userData->id:'';
                                 </span>
                             </h3>
                             <div class="myqTodayGraphSec">
-                                <input type="hidden" name="accurate_percent" id="accurate_percent" value="50">
-                                <div class="mq_circle_percent" data-percent="50">
-                                    <div class="circletop" style="border-radius: 100%;overflow:hidden;">
+                                <input type="hidden" name="accurate_percent" id="accurate_percent" value="{{$myqtodayScore}}">
+                                <div class="mq_circle_percent" data-percent="{{$accurate_percent}}">
+                                    <div class="circletop">
                                         <div class="mq_circle_inner">
                                             <div class="mq_round_per"></div>
                                         </div>
