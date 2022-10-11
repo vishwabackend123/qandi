3e02f7bb81ed2d72d7f843676dcd53936b031a10
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-10-11 13:52:02 +0530

    redis testing

--- config/database.php
+++ config/database.php
@@ -25,6 +25,7 @@ if (isset($resultStudent['SecretString']) && !empty($resultStudent['SecretString
     $redis_data=json_decode($resultStudent['SecretString'], true);
 };
 
+
 return [
 
     /*
@@ -150,11 +151,11 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', $redis_data['REDIS_HOST']),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', $redis_data['REDIS_PORT']),
-            'database' => env('REDIS_DB', '0'),
+            'url' => env('REDIS_URL',$redis_data['CACHE_URL']),
+            #'host' => env('REDIS_HOST', $redis_data['REDIS_HOST']),
+            #'password' => env('REDIS_PASSWORD', null),
+            #'port' => env('REDIS_PORT', $redis_data['REDIS_PORT']),
+            #'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
