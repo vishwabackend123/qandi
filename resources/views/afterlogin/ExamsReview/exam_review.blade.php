8402e4fb48d4e67eedabd63682543a18b4589c92
sachin <sachinawasthi1989@gmail.com>
2022-10-13 12:42:49 +0530

    review screen space issue

--- resources/views/afterlogin/ExamsReview/exam_review.blade.php
+++ resources/views/afterlogin/ExamsReview/exam_review.blade.php
@@ -945,14 +945,13 @@ $question_type = "Numerical";
                 $('.hideyes').show(0);
                 $('.overlaydiv').show(0);
                 var examreviewMaincontainerouter = $(".examreviewMaincontainer").outerHeight();
-
-
-
+                $('.custom-anstop').attr('style', 'opacity:1 !important;');
                 var examreviewMainbox = examreviewMaincontainerouter - 250 + "px";
                 $('.custom-anstop').css('height', examreviewMainbox);
                 var customanstopheightNew = $(".custom-anstop").outerHeight();
                 var examreviewMainboxNew = customanstopheightNew - 70 + "px";
                 $('.text-exambottom-sec').css('height', examreviewMainboxNew);
+               
 
 
 
@@ -964,7 +963,7 @@ $question_type = "Numerical";
                 $('.hideyes').hide(0);
                 $('.overlaydiv').hide(0);
 
-                $('.custom-anstop').attr('style', 'height:0px !important;');
+                $('.custom-anstop').attr('style', 'height:0px !important; opacity:0;');
                 $('.text-exambottom-sec').attr('style', 'height: 0px !important; ');
 
             });
