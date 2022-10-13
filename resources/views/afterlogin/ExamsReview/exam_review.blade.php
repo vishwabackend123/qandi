ca8eb969c8a6c6045b3dabb87f709538bd6ec3a3
sachin <sachinawasthi1989@gmail.com>
2022-10-13 13:06:05 +0530

    put dispaly none in review page

--- resources/views/afterlogin/ExamsReview/exam_review.blade.php
+++ resources/views/afterlogin/ExamsReview/exam_review.blade.php
@@ -945,7 +945,7 @@ $question_type = "Numerical";
                 $('.hideyes').show(0);
                 $('.overlaydiv').show(0);
                 var examreviewMaincontainerouter = $(".examreviewMaincontainer").outerHeight();
-                $('.custom-anstop').attr('style', 'opacity:1 !important;');
+                $('.custom-anstop').attr('style', 'opacity:1 !important; display:block !important;');
                 var examreviewMainbox = examreviewMaincontainerouter - 250 + "px";
                 $('.custom-anstop').css('height', examreviewMainbox);
                 var customanstopheightNew = $(".custom-anstop").outerHeight();
@@ -963,7 +963,7 @@ $question_type = "Numerical";
                 $('.hideyes').hide(0);
                 $('.overlaydiv').hide(0);
 
-                $('.custom-anstop').attr('style', 'height:0px !important; opacity:0;');
+                $('.custom-anstop').attr('style', 'height:0px !important; opacity:0; display:none;');
                 $('.text-exambottom-sec').attr('style', 'height: 0px !important; ');
 
             });
@@ -987,6 +987,9 @@ $question_type = "Numerical";
 
         }
         .removemob{
+            display:none;
+        }
+        .custom-anstop{
             display:none;
         }
     }
