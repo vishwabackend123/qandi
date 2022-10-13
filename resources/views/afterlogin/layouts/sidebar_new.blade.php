9108012ba987d61638952724fb90f07fd3636645
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-10-13 14:21:31 +0530

    active tab

--- resources/views/afterlogin/layouts/sidebar_new.blade.php
+++ resources/views/afterlogin/layouts/sidebar_new.blade.php
@@ -28,7 +28,7 @@ $userData = Session::get('user_data');
             </svg>
         </a>
         <ul class="mob_sidebar_lists m-0">
-            <li class="active">
+            <li class="active current_dashboard">
                 <a href="{{ url('/dashboard') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
                         <path d="M25.667 9.94V4.643c0-1.645-.747-2.31-2.602-2.31h-4.713c-1.855 0-2.602.665-2.602 2.31v5.285c0 1.657.747 2.31 2.602 2.31h4.713c1.855.012 2.602-.653 2.602-2.298zM25.667 23.065v-4.713c0-1.855-.747-2.602-2.602-2.602h-4.713c-1.855 0-2.602.747-2.602 2.602v4.713c0 1.855.747 2.602 2.602 2.602h4.713c1.855 0 2.602-.747 2.602-2.602zM12.25 9.94V4.643c0-1.645-.746-2.31-2.601-2.31H4.934c-1.855 0-2.601.665-2.601 2.31v5.285c0 1.657.746 2.31 2.601 2.31H9.65c1.855.012 2.601-.653 2.601-2.298zM12.25 23.065v-4.713c0-1.855-.746-2.602-2.601-2.602H4.934c-1.855 0-2.601.747-2.601 2.602v4.713c0 1.855.746 2.602 2.601 2.602H9.65c1.855 0 2.601-.747 2.601-2.602z" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -39,7 +39,7 @@ $userData = Session::get('user_data');
                     <span>Dashboard</span>
                 </a>
             </li>
-            <li>
+            <li class="current_practice">
                 <a href="javascript:void(0)" class="testsubmenu_open">
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
                         <path d="M15.471 4.2 5.893 14.34c-.362.385-.712 1.143-.782 1.668l-.431 3.78c-.152 1.365.828 2.298 2.181 2.065l3.757-.642c.525-.093 1.26-.478 1.621-.875l9.579-10.138c1.656-1.75 2.403-3.745-.175-6.183-2.567-2.415-4.515-1.564-6.172.186z" stroke="#234628" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
@@ -52,7 +52,7 @@ $userData = Session::get('user_data');
                     <span>Assessments</span>
                 </a>
             </li>
-            <li>
+            <li class="current_analytics">
                 <a href="{{route('overall_analytics')}}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                         <path d="M21.21 15.89A10 10 0 1 1 8 2.83" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -65,7 +65,7 @@ $userData = Session::get('user_data');
                     <span>Analytics</span>
                 </a>
             </li>
-            <li>
+            <li class="current_planner">
                 <a href="{{ url('/planner') }}">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M19 4H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zM16 2v4M8 2v4M3 10h18" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -76,7 +76,7 @@ $userData = Session::get('user_data');
                     <span>Planner</span>
                 </a>
             </li>
-            <li>
+            <li class="current_refer">
                 <a href="#referfrnd" data-bs-toggle="modal">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                         <path d="M18 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM6 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM18 22a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM8.59 13.51l6.83 3.98M15.41 6.51l-6.82 3.98" stroke="#234628" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
