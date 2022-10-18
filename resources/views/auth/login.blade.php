b4c27a44736078267ec1cb8681acd80859856e00
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-10-18 13:02:04 +0530

    spacial char

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -345,6 +345,7 @@
         function isNumber(evt) {
             evt = (evt) ? evt : window.event;
             var charCode = (evt.which) ? evt.which : evt.keyCode;
+            alert(charCode);
             if (charCode > 31 && (charCode < 48 || charCode > 57)) {
                 return false;
             }
