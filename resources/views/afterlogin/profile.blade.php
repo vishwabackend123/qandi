73b786ea322aef217ddd213c7dd9bc5942984ddb
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-10-19 17:55:30 +0530

    validate input

--- resources/views/afterlogin/profile.blade.php
+++ resources/views/afterlogin/profile.blade.php
@@ -62,13 +62,13 @@ $user_id = isset($userData->id)?$userData->id:'';
                                 <div class="col-lg-6">
                                     <div class="custom-input pb-4">
                                         <label>First Name</label>
-                                        <input type="text" class="form-control txtOnly" placeholder="First Name" value="{{$userData->first_name}}" id="firstname" name="firstname" onkeypress="return onlyAlphabetsForName(event,this);" onkeyup="onlyAlphabetsAccept(event);" maxlength="15" onpaste="validatePaste(this, event)">
+                                        <input type="text" class="form-control txtOnly" placeholder="First Name" value="{{$userData->first_name}}" id="firstname" name="firstname" onkeypress="return onlyAlphabetsForName(event,this);"  maxlength="15" onpaste="validatePaste(this, event)">
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
                                     <div class="custom-input pb-4">
                                         <label>Last Name</label>
-                                        <input type="text" class="form-control" placeholder="Last Name" value="{{$userData->last_name}}" id="lastname" name="lastname" onkeypress="return onlyAlphabetsForName(event,this);" maxlength="15" onpaste="validatePaste(this, event)">
+                                        <input type="text" class="form-control txtOnly" placeholder="Last Name" value="{{$userData->last_name}}" id="lastname" name="lastname" onkeypress="return onlyAlphabetsForName(event,this);" maxlength="15" onpaste="validatePaste(this, event)">
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
@@ -484,13 +484,9 @@ $user_id = isset($userData->id)?$userData->id:'';
             }
 
         });
-        function onlyAlphabetsAccept(e){
-            //alert(e.target.value);
-
-        }
         $('.txtOnly').bind('keyup blur',function(){ 
             var node = $(this);
-            node.val(node.val().replace(/[^a-z]/g,'') ); }
+            node.val(node.val().replace(/[^a-zA-Z]/g,'') ); }
         );
     </script>
 </body>
