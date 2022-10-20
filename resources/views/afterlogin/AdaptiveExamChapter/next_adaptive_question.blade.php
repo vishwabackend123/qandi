42324ca7f8bb9ef8ffd587ccb6ef951251ff0a0b
sachin <sachinawasthi1989@gmail.com>
2022-10-20 11:58:18 +0530

    test screen extrta space

--- resources/views/afterlogin/AdaptiveExamChapter/next_adaptive_question.blade.php
+++ resources/views/afterlogin/AdaptiveExamChapter/next_adaptive_question.blade.php
@@ -97,7 +97,7 @@ $question_type = "Numerical";
 </script>
 <div class="questionType">
     <div class="questionTypeinner">
-        <div class="questionChoiceType" style="visibility:hidden">
+        <div class="questionChoiceType questionChoiceTypehide" style="visibility:hidden">
             <div class="questionChoice"><a class="singleChoice" href="javascript:;">Section A (20Q) - Single Choice</a> <a class="numericalChoice" href="javascript:;">Section B (10Q) - Numerical</a></div>
         </div>
         <div class="timeCounter">
