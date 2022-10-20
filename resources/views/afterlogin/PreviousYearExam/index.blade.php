25de8bb03ad146acc27a7a38640346fe4d7e4b34
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-10-20 20:34:21 +0530

    remove condition

--- resources/views/afterlogin/PreviousYearExam/index.blade.php
+++ resources/views/afterlogin/PreviousYearExam/index.blade.php
@@ -202,14 +202,7 @@
             $(this).text('View details');
         }
     });
-   $(document).click(function (e) {
-     e.stopPropagation();
-     var container = $(".customDropdown");
-     if (container.has(e.target).length === 0) {
-        alert("hi");
-         $('.customDropdown').removeClass('active');
-     };
-})
+
 </script>
 @include('afterlogin.layouts.footer_new')
 @endsection
