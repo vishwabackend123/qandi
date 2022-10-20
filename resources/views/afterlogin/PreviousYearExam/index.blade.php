e719692f6158a302c573d7a98c560b7e2e3651fe
Shubham <sakshi707>
2022-10-20 13:27:06 +0530

    dropdown

--- resources/views/afterlogin/PreviousYearExam/index.blade.php
+++ resources/views/afterlogin/PreviousYearExam/index.blade.php
@@ -202,12 +202,17 @@
             $(this).text('View details');
         }
     });
-    $(document).click(function (e) {
+    $(document).ready(function (e) {
     e.stopPropagation();
     var container = $(".customDropdown");
     if (container.has(e.target).length === 0) {
         $('.customDropdown').removeClass('active');
-    }
+    };
+    $("#filter_year").on("blur", function() {
+        alert('aa');
+        $('.customDropdown').removeClass('active');
+
+   });
 })
 </script>
 @include('afterlogin.layouts.footer_new')
