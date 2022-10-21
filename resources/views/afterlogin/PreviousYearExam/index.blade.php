fb0060310f7ef5cbf980a4024e884c4db8851680
My Name <sakshi707>
2022-10-21 12:25:05 +0530

    prev year dropdown

--- resources/views/afterlogin/PreviousYearExam/index.blade.php
+++ resources/views/afterlogin/PreviousYearExam/index.blade.php
@@ -58,7 +58,7 @@
                                         <p class="previous_exam_year_exam_paper_text"><span class="mobile_hide">Test your preparedness with </span>Past year exam papers</p>
                                     </div>
                                     <div class="dropbox previous_dropbox_page">
-                                        <div class="customDropdown dropdown">
+                                        <!-- <div class="customDropdown dropdown">
                                             @php
                                             $latest_year = date('Y');
                                             @endphp
@@ -70,6 +70,18 @@
                                                 @endforeach
                                                 @endif
                                             </select>
+                                        </div> -->
+                                        <div class="customDropdown1 dropdown" id="dropdown1">
+                                            <input class="text-box markstrend" type="text" id="markstrend_graph2" placeholder="Select Year" readonly>
+                                            <div class="options">
+                                                <div style=" overflow-y: auto;  height: 145px;">
+                                                    <div class="active markstrend">All Test</div>
+                                                    <div class="markstrend">Mock Test</div>
+                                                    <div class="markstrend">Practice Test</div>
+                                                    <div class="markstrend">Test Series</div>
+                                                    <div class="markstrend">Live Test</div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -202,6 +214,132 @@
             $(this).text('View details');
         }
     });
+
+    let dropdown1 = document.querySelector("#dropdown1")
+        dropdown1.onclick = function() {
+            dropdown1.classList.toggle("active1")
+        }
+
+
 </script>
 @include('afterlogin.layouts.footer_new')
+
+<style>
+        .customDropdown1 {
+            position: relative;
+            width: 100%;
+            border-radius: 10px;
+            height: 60px;
+        }
+
+        .customDropdown1::before {
+            content: "";
+            background: url(https://app.thomsondigital2021.com/public/after_login/current_ui/images/arrow_drop_down.svg);
+            position: absolute;
+            top: 27px;
+            right: 20px;
+            z-index: 1;
+            width: 21px;
+            height: 8px;
+            transition: 0.5s;
+            pointer-events: none;
+            background-size: revert;
+            background-position: center;
+            width: 13.1px;
+            height: 10px;
+        }
+
+        .customDropdown1.active1::before {
+            top: 22px;
+            transform: rotate(-180deg);
+        }
+
+        .customDropdown1 input {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            cursor: pointer;
+            background: #f6f9fd;
+            border: none;
+            outline: none;
+            box-shadow: 0 5px 20px rgba(0, 0, 0, 0.05);
+            padding: 12px 20px;
+            border-radius: 10px;
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 1.6;
+            letter-spacing: normal;
+        }
+
+        .customDropdown1 .options {
+            position: absolute;
+            top: 70px;
+            width: 100%;
+            background: #fff;
+            box-shadow: 0 5px 20px rgba(0, 0, 0, 0.05);
+            border-radius: 10px;
+            -moz-border-radius: 10px;
+            -webkit-border-radius: 10px;
+            -moz-scrollbar-position: outside;
+            overflow: hidden;
+            display: none;
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 1.6;
+            letter-spacing: normal;
+            color: #1f1f1f;
+        }
+
+        .customDropdown1.active1 .options {
+            display: block;
+            z-index: 9;
+        }
+
+        .customDropdown1 .options .markstrend {
+            padding: 12px 20px;
+            cursor: pointer;
+        }
+
+        .journeyBoxcontainer .customDropdown1 .options .markstrend:hover {
+            background: #f0fcf2;
+        }
+
+        .customDropdownpdown1 input::-webkit-input-placeholder {
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 1.6;
+            text-align: left;
+            color: #1f1f1f;
+        }
+
+        .customDropdown1 input::-webkit-input-placeholder {
+            /* Edge */
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 1.6;
+            text-align: left;
+            color: #1f1f1f;
+        }
+
+        .customDropdown1 input:-ms-input-placeholder {
+            /* Internet Explorer 10-11 */
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 1.6;
+            text-align: left;
+            color: #1f1f1f;
+        }
+
+        .customDropdown1 input::placeholder {
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 1.6;
+            text-align: left;
+            color: #1f1f1f;
+        }
+    </style>
+
 @endsection
+
