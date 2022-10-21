5514466239b38614b16acf77a8e8ca6ed9e22046
My Name <sakshi707>
2022-10-21 17:29:51 +0530

    label changed

--- resources/views/afterlogin/Analytics/overall_analytics.blade.php
+++ resources/views/afterlogin/Analytics/overall_analytics.blade.php
@@ -336,7 +336,7 @@
                                                 <div class="tab-pane fade show active" id="pills-Day2" role="tabpanel" aria-labelledby="pills-Day2-tab">
                                                     <div class="chartspent  bargraph_scroll">
                                                         <div class="graph_padd bargraph_size">
-                                                            <span class="yaxis_label"><small>Marks </small> </span>
+                                                            <span class="yaxis_label yaxis_label_3"><small>No. of Answers </small> </span>
                                                             <canvas id="mark_trend_day"></canvas>
                                                         </div>
                                                     </div>
@@ -344,7 +344,7 @@
                                                 <div class="tab-pane fade" id="pills-Week2" role="tabpanel" aria-labelledby="pills-Week2-tab">
                                                     <div class="chartspent  bargraph_scroll">
                                                         <div class="graph_padd bargraph_size">
-                                                            <span class="yaxis_label"><small> Marks </small> </span>
+                                                            <span class="yaxis_label yaxis_label_3"><small> No. of Answers </small> </span>
                                                             <canvas id="mark_trend_week"></canvas>
                                                         </div>
                                                     </div>
@@ -352,7 +352,7 @@
                                                 <div class="tab-pane fade" id="pills-Month2" role="tabpanel" aria-labelledby="pills-Month2-tab">
                                                     <div class="chartspent  bargraph_scroll">
                                                         <div class="graph_padd bargraph_size">
-                                                            <span class="yaxis_label"><small> Marks </small> </span>
+                                                            <span class="yaxis_label yaxis_label_3"><small> No. of Answers </small> </span>
                                                             <canvas id="mark_trend_month"></canvas>
                                                         </div>
                                                     </div>
