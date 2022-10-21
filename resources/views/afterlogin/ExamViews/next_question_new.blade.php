56f54df391ea6b5f836b76a54dbff02cdc955252
dinesh@ahom.tech-at-225208115870 <dinesh@ahom.tech-at-225208115870>
2022-10-21 17:50:52 +0530

    test series testing

--- resources/views/afterlogin/ExamViews/next_question_new.blade.php
+++ resources/views/afterlogin/ExamViews/next_question_new.blade.php
@@ -268,39 +268,89 @@ $question_type = "Numerical";
     });
 
     /* Allow only numeric with decimal */
-    $('.allownumericwithdecimal').bind("cut copy paste", function(e) {
-        e.preventDefault();
-    })
-    $(".allownumericwithdecimal").on("keypress keyup blur", function(event) {
+    jQuery(function($) {
 
-        //this.value = this.value.replace(/[^0-9\.]/g,'');
-        $(this).val($(this).val().replace(/(?!^-)[^0-9.]/g, ''));
-        if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 45 || event.which > 57 || event.which == 47)) {
-            event.preventDefault();
-        }
-        var text = $(this).val();
-        if ((text.indexOf('.') != -1) && (text.substring(text.indexOf('.')).length > 2) && (event.which != 0 && event.which != 8) && ($(this)[0].selectionStart >= text.length - 2)) {
-            event.preventDefault();
-        }
-        if (text.indexOf('.') > -1) {
-            var textarray = text.split('.');
-            if (textarray[1].length === 2) {
-                event.preventDefault();
-            }
-        }
-        if (event.charCode === 46) {
-            // if dot is the first symbol
-            if (event.target.value.length === 0) {
-                event.preventDefault();
-                return;
-            }
+        $('.allownumericwithdecimal').bind("cut copy paste", function(e) {
+            e.preventDefault();
+        });
+        var newWindowWidth = $(window).width();
+        if (newWindowWidth < 768) {
 
-            // if there are dots already 
-            if (event.target.value.indexOf('.') !== -1) {
-                event.preventDefault();
-                return;
-            }
 
+            $('textarea.allownumericwithdecimal').on('textInput', event => {
+                var self = $(this);
+                self.val(self.val().replace(/(?!^-)[^0-9\.]/g, ""));
+
+
+                var keyCode = event.originalEvent.data.charCodeAt(0);
+
+                if ((keyCode != 46 || $(this).val().indexOf('.') != -1) && (keyCode < 45 || keyCode > 57 || keyCode == 47 || keyCode == 229)) {
+
+                    event.preventDefault();
+                } else {
+                    let textinput = $('.allownumericwithdecimal').val();
+                    if (textinput.indexOf('.') > -1) {
+                        var textarray = textinput.split('.');
+                        if (textarray[1].length === 2) {
+                            event.preventDefault();
+                        }
+                    }
+                    if (keyCode === 46) {
+                        // if dot is the first symbol
+                        if (event.target.value.length === 0) {
+                            event.preventDefault();
+                        }
+
+                        // if there are dots already 
+                        if (event.target.value.indexOf('.') !== -1) {
+                            event.preventDefault();
+                        }
+                    }
+                    if (keyCode === 45) {
+                        // if - more than 1
+
+                        if (event.target.value.length > 0) {
+                            event.preventDefault();
+                        }
+                    }
+                }
+
+
+            });
+        } else {
+            $(".allownumericwithdecimal").on("keypress", function(event) {
+
+
+                //this.value = this.value.replace(/[^0-9\.]/g,'');
+                $(this).val($(this).val().replace(/(?!^-)[^0-9.]/g, ''));
+                if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 45 || event.which > 57 || event.which == 47)) {
+
+                    event.preventDefault();
+                }
+                let text = $(this).val();
+                if ((text.indexOf('.') != -1) && (text.substring(text.indexOf('.')).length > 2) && (event.which != 0 && event.which != 8) && ($(this)[0].selectionStart >= text.length - 2)) {
+
+                    event.preventDefault();
+                }
+
+
+                if (event.charCode === 46) {
+
+                    // if dot is the first symbol
+                    if (event.target.value.length === 0) {
+                        event.preventDefault();
+
+                    }
+
+                    // if there are dots already 
+                    if (event.target.value.indexOf('.') !== -1) {
+                        event.preventDefault();
+
+                    }
+
+                }
+
+            });
         }
     });
 
