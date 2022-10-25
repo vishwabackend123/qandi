f7a705f071e6d97c97a41ed188e769574b0bb18d
dinesh@ahom.tech-at-225208115870 <dinesh@ahom.tech-at-225208115870>
2022-10-25 14:34:54 +0530

    os detection

--- resources/views/afterlogin/MockExam/next_mock_question.blade.php
+++ resources/views/afterlogin/MockExam/next_mock_question.blade.php
@@ -261,6 +261,31 @@ $question_type = "Numerical";
     }
 </script>
 <script>
+    function getOS() {
+        var userAgent = window.navigator.userAgent,
+            platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+            macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+            windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+            iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+            os = null;
+
+        if (macosPlatforms.indexOf(platform) !== -1) {
+            os = 'Mac OS';
+        } else if (iosPlatforms.indexOf(platform) !== -1) {
+            os = 'iOS';
+        } else if (windowsPlatforms.indexOf(platform) !== -1) {
+            os = 'Windows';
+        } else if (/Android/.test(userAgent)) {
+            os = 'Android';
+        } else if (/Linux/.test(platform)) {
+            os = 'Linux';
+        }
+
+        return os;
+    }
+
+    alert(getOS());
+
     $(document).ready(function() {
         $(window).on("resize", function(e) {
             checkScreenSize();
