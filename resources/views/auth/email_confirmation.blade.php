8eb9662c7c7705b476892446e3b5e2733cca30d1
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2022-10-31 17:20:11 +0530

    email issue

--- resources/views/auth/email_confirmation.blade.php
+++ resources/views/auth/email_confirmation.blade.php
@@ -87,7 +87,11 @@
     </div>
     @endif
 </div>
-<?php $redis_data = Session::get('redis_data'); ?>
+<?php 
+    if( isset( $_SESSION['SECRET_REDIS'] ) ) {
+      $redis_data = $_SESSION['SECRET_REDIS'];
+   }
+?>
 <script type="text/javascript">
 
     // Mixpanel Started
@@ -113,7 +117,7 @@ $('.email_send').hide();
 
     // Mixpanel Event Ended
 </script>
-
+@endif
 
 @if(isset($response_json['student_id']))
 <script type="text/javascript">
