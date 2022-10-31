a5abfaa3a36bc0670e06d5859270d367c41e8edf
dinesh@ahom.tech-at-225208115870 <dinesh@ahom.tech-at-225208115870>
2022-10-31 18:34:32 +0530

    planner error display multiple click issue

--- resources/views/afterlogin/planner/planner_schedule.blade.php
+++ resources/views/afterlogin/planner/planner_schedule.blade.php
@@ -15,19 +15,65 @@ $userData = Session::get('user_data');
 <?php $redis_data = Session::get('redis_data'); ?>
 <!-- Mixpanel Started -->
 <script type="text/javascript">
-(function(f,b){if(!b.__SV){var e,g,i,h;window.mixpanel=b;b._i=[];b.init=function(e,f,c){function g(a,d){var b=d.split(".");2==b.length&&(a=a[b[0]],d=b[1]);a[d]=function(){a.push([d].concat(Array.prototype.slice.call(arguments,0)))}}var a=b;"undefined"!==typeof c?a=b[c]=[]:c="mixpanel";a.people=a.people||[];a.toString=function(a){var d="mixpanel";"mixpanel"!==c&&(d+="."+c);a||(d+=" (stub)");return d};a.people.toString=function(){return a.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
-for(h=0;h<i.length;h++)g(a,i[h]);var j="set set_once union unset remove delete".split(" ");a.get_group=function(){function b(c){d[c]=function(){call2_args=arguments;call2=[c].concat(Array.prototype.slice.call(call2_args,0));a.push([e,call2])}}for(var d={},e=["get_group"].concat(Array.prototype.slice.call(arguments,0)),c=0;c<j.length;c++)b(j[c]);return d};b._i.push([e,f,c])};b.__SV=1.2;e=f.createElement("script");e.type="text/javascript";e.async=!0;e.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?
-MIXPANEL_CUSTOM_LIB_URL:"file:"===f.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";g=f.getElementsByTagName("script")[0];g.parentNode.insertBefore(e,g)}})(document,window.mixpanel||[]);
-
-// Enabling the debug mode flag is useful during implementation,
-// but it's recommended you remove it for production
-var mixpanelid="{{$redis_data['MIXPANEL_KEY']}}";
-mixpanel.init(mixpanelid);
-mixpanel.track('Loaded Planner',{
-        "$city" : '<?php echo $userData->city; ?>',
+    (function(f, b) {
+        if (!b.__SV) {
+            var e, g, i, h;
+            window.mixpanel = b;
+            b._i = [];
+            b.init = function(e, f, c) {
+                function g(a, d) {
+                    var b = d.split(".");
+                    2 == b.length && (a = a[b[0]], d = b[1]);
+                    a[d] = function() {
+                        a.push([d].concat(Array.prototype.slice.call(arguments, 0)))
+                    }
+                }
+                var a = b;
+                "undefined" !== typeof c ? a = b[c] = [] : c = "mixpanel";
+                a.people = a.people || [];
+                a.toString = function(a) {
+                    var d = "mixpanel";
+                    "mixpanel" !== c && (d += "." + c);
+                    a || (d += " (stub)");
+                    return d
+                };
+                a.people.toString = function() {
+                    return a.toString(1) + ".people (stub)"
+                };
+                i = "disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+                for (h = 0; h < i.length; h++) g(a, i[h]);
+                var j = "set set_once union unset remove delete".split(" ");
+                a.get_group = function() {
+                    function b(c) {
+                        d[c] = function() {
+                            call2_args = arguments;
+                            call2 = [c].concat(Array.prototype.slice.call(call2_args, 0));
+                            a.push([e, call2])
+                        }
+                    }
+                    for (var d = {}, e = ["get_group"].concat(Array.prototype.slice.call(arguments, 0)), c = 0; c < j.length; c++) b(j[c]);
+                    return d
+                };
+                b._i.push([e, f, c])
+            };
+            b.__SV = 1.2;
+            e = f.createElement("script");
+            e.type = "text/javascript";
+            e.async = !0;
+            e.src = "undefined" !== typeof MIXPANEL_CUSTOM_LIB_URL ?
+                MIXPANEL_CUSTOM_LIB_URL : "file:" === f.location.protocol && "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//) ? "https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js" : "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";
+            g = f.getElementsByTagName("script")[0];
+            g.parentNode.insertBefore(e, g)
         }
-);
+    })(document, window.mixpanel || []);
 
+    // Enabling the debug mode flag is useful during implementation,
+    // but it's recommended you remove it for production
+    var mixpanelid = "{{$redis_data['MIXPANEL_KEY']}}";
+    mixpanel.init(mixpanelid);
+    mixpanel.track('Loaded Planner', {
+        "$city": '<?php echo $userData->city; ?>',
+    });
 </script>
 <!-- Mixpanel Event Ended -->
 
@@ -224,6 +270,10 @@ mixpanel.track('Loaded Planner',{
 
 
     function increaseValue() {
+        $('#limit_error span').html('');
+
+        $('#limit_error').hide();
+
         if ($('#StartDate').val() == '') {
             $("#saveplannerbutton").click();
             return false;
@@ -257,6 +307,9 @@ mixpanel.track('Loaded Planner',{
     }
 
     function decreaseValue() {
+        $('#limit_error span').html('');
+        $('#limit_error').hide();
+
         if ($('#StartDate').val() == '') {
             $("#saveplannerbutton").click();
             return false;
@@ -305,18 +358,15 @@ mixpanel.track('Loaded Planner',{
 
 
         if (limit == 0) {
-            if ($('#limit_error').is(":visible")) {
-                $('#limit_error').fadeOut(5000);
 
-            }
             $('#limit_error span').html('');
             var error_txt = 'Please select Exams Per Week';
             $('#limit_error span').html(error_txt);
             $('#limit_error').show();
             $(".planner-box")[0].scrollIntoView();;
-            setTimeout(function() {
-                $('#limit_error ').fadeOut('fast');
-            }, 3000);
+            /*  setTimeout(function() {
+                 $('#limit_error').fadeOut('fast');
+             }, 3000); */
             return false;
         }
 
@@ -328,17 +378,14 @@ mixpanel.track('Loaded Planner',{
             } else {
                 var error_txt = 'You can not select more than ' + limit + ' chapters for selected week';
             }
-            if ($('#limit_error').is(":visible")) {
-                $('#limit_error').fadeOut(5000);
 
-            }
 
             $('#limit_error span').html(error_txt);
             $('#limit_error').show();
             $(".planner-box")[0].scrollIntoView();;
-            setTimeout(function() {
-                $('#limit_error').fadeOut('fast');
-            }, 3000);
+            /*  setTimeout(function() {
+                 $('#limit_error').fadeOut('fast');
+             }, 3000); */
             return false;
         }
 
@@ -490,9 +537,9 @@ mixpanel.track('Loaded Planner',{
                 $('#limit_error span').html(error_txt);
                 $('#limit_error').show();
                 $(".planner-box")[0].scrollIntoView();;
-                setTimeout(function() {
-                    $('#limit_error').fadeOut('fast');
-                }, 3000);
+                /*  setTimeout(function() {
+                     $('#limit_error').fadeOut('fast');
+                 }, 3000); */
                 return false;
             }
             if (limit <= 0) {
@@ -500,9 +547,9 @@ mixpanel.track('Loaded Planner',{
                 $('#limit_error span').html('Please set at least one exam for the selected week.');
                 $('#limit_error').show();
                 $(".planner-box")[0].scrollIntoView();;
-                setTimeout(function() {
+                /* setTimeout(function() {
                     $('#limit_error').fadeOut('fast');
-                }, 3000);
+                }, 3000); */
                 return false;
             }
             var chapters = [];
